6de3d04ca1bd34b06620e1e3f139a8c88aabaf83
Carlos && Ponce <srtager555@gmail.com>
2024-12-20 12:40:45 -0600

    logic to set custom layouts added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
+import { Layout } from "@/components/layouts/index.layout";
 import "@/styles/globals.css";
+import { NextPage } from "next";
 import type { AppProps } from "next/app";
+import { ReactElement, ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout ?? ((page) => page);
+
+  return (
+    <>
+      <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
+    </>
+  );
 }

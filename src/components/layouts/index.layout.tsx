c0233e4230ec09304ab29832e0466ce1023e8a29
Carlos && Ponce <srtager555@gmail.com>
2024-12-23 22:31:06 -0600

    padding added to the layout

--- src/components/layouts/index.layout.tsx
+++ src/components/layouts/index.layout.tsx
@@ -1,10 +1,13 @@
 import { InitApp } from "@/firebase/InitApp";
 import { NavLayout } from "./nav.layout";
+import { Container } from "@/styles/index.styles";
 
 export function Layout({ children }: { children: children }) {
   return (
     <InitApp>
-      <NavLayout>{children}</NavLayout>
+      <NavLayout>
+        <Container styles={{ padding: "25px" }}>{children}</Container>
+      </NavLayout>
     </InitApp>
   );
 }

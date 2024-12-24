740b29249d9d2c202b24258f16b778e88514b8ae
Carlos && Ponce <srtager555@gmail.com>
2024-12-23 22:25:11 -0600

    logic to get the icons updated

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,16 +1,29 @@
 import SellerIcon from "@/../public/icons/sellers_light_50.svg";
 import ProductIcon from "@/../public/icons/product_icon.svg";
+import InvoiceIcon from "@/../public/icons/invoice_icon.svg";
+import ChartsIcon from "@/../public/icons/chart_icon.svg";
+import HomeIcon from "@/../public/icons/home_icon.svg";
 import Image from "next/image";
+import { ImageProps, StaticImport } from "next/dist/shared/lib/get-img-props";
 
-type ImageProps = {
-  height?: number;
-  width?: number;
+type iconType = "seller" | "product" | "invoice" | "chart" | "home";
+
+interface I
+  extends Omit<ImageProps, "src" | "alt">,
+    Partial<Pick<ImageProps, "src" | "alt">> {
+  iconType: iconType;
+}
+
+export const Icon = (props: I) => {
+  const src: Record<iconType, StaticImport> = {
+    home: HomeIcon,
+    seller: SellerIcon,
+    product: ProductIcon,
+    invoice: InvoiceIcon,
+    chart: ChartsIcon,
+  };
+
+  return (
+    <Image height={30} width={30} {...props} src={src[props.iconType]} alt="" />
+  );
 };
-
-export const ProductsIcon = ({ height, width }: ImageProps) => (
-  <Image src={ProductIcon} height={height || 35} width={width || 35} alt="" />
-);
-
-export const SellersIcon = ({ height, width }: ImageProps) => (
-  <Image src={SellerIcon} height={height || 35} width={width || 35} alt="" />
-);

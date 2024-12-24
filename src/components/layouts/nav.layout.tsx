5197aaf2f59e7b8fd20556326f87b61c705742a9
Carlos && Ponce <srtager555@gmail.com>
2024-12-23 22:31:24 -0600

    Anchors updated

--- src/components/layouts/nav.layout.tsx
+++ src/components/layouts/nav.layout.tsx
@@ -1,10 +1,25 @@
 import { Nav, NavAnchor, NavContainer } from "@/styles/Nav.module";
+import { Icon } from "../Icons";
 
 export function NavLayout({ children }: { children: children }) {
   return (
     <NavContainer deployNav={false}>
       <Nav>
-        <NavAnchor href={""}>Awa</NavAnchor>
+        <NavAnchor href={"/feed"}>
+          <Icon iconType="home" />
+        </NavAnchor>
+        <NavAnchor href={"/sellers"}>
+          <Icon iconType="seller" />
+        </NavAnchor>
+        <NavAnchor href={"/products"}>
+          <Icon iconType="product" />
+        </NavAnchor>
+        <NavAnchor href={"/invoices"}>
+          <Icon iconType="invoice" />
+        </NavAnchor>
+        <NavAnchor href={"/invoices"}>
+          <Icon iconType="chart" />
+        </NavAnchor>
       </Nav>
       {children}
     </NavContainer>

dd5e673476e7d498b4d2b9b60116e568d7c68253
Carlos && Ponce <srtager555@gmail.com>
2024-12-23 21:18:43 -0600

    main nav removed§

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,13 +1,7 @@
-import { ReactElement } from "react";
 import type { NextPageWithLayout } from "../_app";
-import { NavLayout } from "@/components/layouts/nav.layout";
 
 const Feed: NextPageWithLayout = () => {
   return <p>There is the Feed</p>;
-};
-
-Feed.getLayout = function getLayout(page: ReactElement) {
-  return <NavLayout>{page}</NavLayout>;
 };
 
 export default Feed;

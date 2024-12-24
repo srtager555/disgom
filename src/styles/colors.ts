e0119e13e5d854733fe0b069e1312463d17c4130
Carlos && Ponce <srtager555@gmail.com>
2024-12-24 13:53:44 -0600

    new var added

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,6 @@
 export const globalCSSVars = {
   "--background": "var(--background)",
   "--foreground": "var(--foreground)",
+  "--foreground-hover": "var(--foreground-hover)",
+  "--detail": "var(--detail)",
 };

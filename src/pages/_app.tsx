29ba871800f6615f8413b45320b31f7d65592c73
Carlos && Ponce <srtager555@gmail.com>
2024-12-23 21:42:55 -0600

    default data removed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,9 +16,5 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return (
-    <>
-      <Layout>{getLayout(<Component {...pageProps} />)}</Layout>
-    </>
-  );
+  return <Layout>{getLayout(<Component {...pageProps} />)}</Layout>;
 }

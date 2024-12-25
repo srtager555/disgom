ccebe0805212f261aad383ab7af5561a2b88dbe2
Carlos && Ponce <srtager555@gmail.com>
2024-12-24 21:41:12 -0600

    custom text input created

---
+++ src/components/Inputs/text.tsx
@@ -0,0 +1,28 @@
+import { Container } from "@/styles/index.styles";
+import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: 5px;
+  font-size: 1rem;
+`;
+
+interface props
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  > {
+  children?: string;
+}
+
+export function InputText({ children, ...props }: props) {
+  return (
+    <Container styles={{ marginBottom: "20px" }}>
+      <p>
+        {children}
+        {props.required && "*"}
+      </p>
+      <Input {...props} />
+    </Container>
+  );
+}

806516af50938856b3ee1c465774a140d2e1344c
Carlos && Ponce <srtager555@gmail.com>
2024-12-24 21:42:07 -0600

    provitional anchor added

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,19 @@
+import { ProductsLayout } from "@/components/layouts/Products.layout";
 import { NextPageWithLayout } from "@/pages/_app";
+import { Container } from "@/styles/index.styles";
+import Link from "next/link";
+import { ReactElement } from "react";
 
 const Page: NextPageWithLayout = () => {
-  return "";
+  return (
+    <Container>
+      <Link href="/products/create">Añadir producto</Link>
+    </Container>
+  );
+};
+
+Page.getLayout = function getLayout(Page: ReactElement) {
+  return <ProductsLayout>{Page}</ProductsLayout>;
 };
 
 export default Page;

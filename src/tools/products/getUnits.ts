b99be0a383fc84f30458e2b5a4df46c2e49a1fb8
Carlos && Ponce <srtager555@gmail.com>
2024-12-24 21:42:58 -0600

    units sorted

--- src/tools/products/getUnits.ts
+++ src/tools/products/getUnits.ts
@@ -1,5 +1,5 @@
 import { productUnits } from "./create";
 
 export function getUnits(): Array<productUnits> {
-  return ["1/2", "1/4", "KG", "LB", "U"];
+  return ["LB", "KG", "U", "1/2", "1/4"];
 }

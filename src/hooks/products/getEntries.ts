a32a1469f2932bbe4d31722f10aabe125d79cd00
Carlos && Ponce <srtager555@gmail.com>
2024-12-28 02:46:44 -0600

    e

---
+++ src/hooks/products/getEntries.ts
@@ -0,0 +1,43 @@
+import { ProductContext } from "@/components/layouts/Products.layout";
+import { ProductsCollection } from "@/tools/firestore/CollectionTyping";
+import { entryDoc } from "@/tools/products/addEntry";
+import { productDoc } from "@/tools/products/create";
+import {
+  collection,
+  CollectionReference,
+  DocumentReference,
+  onSnapshot,
+  query,
+  QueryDocumentSnapshot,
+  QuerySnapshot,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+export type entries = {
+  lastEntry: QueryDocumentSnapshot<entryDoc>;
+  entries: QuerySnapshot<entryDoc>;
+};
+
+export function useGetEntries(ref: DocumentReference<productDoc>) {
+  const [entries, setEntries] = useState<entries>();
+
+  useEffect(() => {
+    const coll = collection(
+      ref,
+      ProductsCollection.entry
+    ) as CollectionReference<entryDoc>;
+
+    const unsubcribe = onSnapshot(coll, (entries) => {
+      setEntries((props) => {
+        return {
+          ...props,
+          entries,
+        };
+      });
+    });
+
+    return () => unsubcribe();
+  }, []);
+
+  return "";
+}

53b7938ef0de38421779a76dd256ca9ccf716995
Carlos && Ponce <srtager555@gmail.com>
2024-12-28 00:55:20 -0600

    fucntion to create entries created

---
+++ src/tools/products/addEntry.ts
@@ -0,0 +1,36 @@
+import {
+  addDoc,
+  collection,
+  doc,
+  DocumentReference,
+  Timestamp,
+} from "firebase/firestore";
+import { productDoc } from "./create";
+import { ProductsCollection } from "../firestore/CollectionTyping";
+import { addToStock } from "./addToStock";
+
+export type entryData = {
+  purchase_price: number;
+  sale_price: number;
+  amount: number;
+};
+
+export interface entryDoc extends entryData {
+  created_at: Timestamp;
+}
+
+export async function addEntry(
+  productRef: DocumentReference<productDoc>,
+  data: entryData
+) {
+  const coll = collection(productRef, ProductsCollection.entry);
+
+  await addDoc(coll, {
+    created_at: new Date(),
+    ...data,
+  }).then(async (_) => {
+    const entry_ref = doc(coll, _.id) as DocumentReference<entryDoc>;
+
+    await addToStock(productRef, { entry_ref, ...data });
+  });
+}

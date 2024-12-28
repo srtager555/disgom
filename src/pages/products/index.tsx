e6b41c218740c591766facba7c20ae0c1e8ab961
Carlos && Ponce <srtager555@gmail.com>
2024-12-28 00:55:48 -0600

    I started to work in the index

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,13 +1,74 @@
-import { ProductsLayout } from "@/components/layouts/Products.layout";
+import { InputNumber } from "@/components/Inputs/number";
+import {
+  ProductContext,
+  ProductsLayout,
+} from "@/components/layouts/Products.layout";
 import { NextPageWithLayout } from "@/pages/_app";
-import { Container } from "@/styles/index.styles";
-import Link from "next/link";
-import { ReactElement } from "react";
+import { Button, Form } from "@/styles/Form.styles";
+import { Container, FlexContainer } from "@/styles/index.styles";
+import { addEntry } from "@/tools/products/addEntry";
+import { FormEvent, ReactElement, useContext } from "react";
 
 const Page: NextPageWithLayout = () => {
+  const { selectedProduct } = useContext(ProductContext);
+
+  const handlerOnSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!selectedProduct) return;
+
+    const { productCostPrice, productSalePrice, amount } =
+      e.target as EventTarget & {
+        productCostPrice: HTMLInputElement;
+        productSalePrice: HTMLInputElement;
+        amount: HTMLInputElement;
+      };
+
+    const cost_price = Number(productSalePrice.value);
+    const sale_price = Number(productCostPrice.value);
+
+    await addEntry(selectedProduct?.ref, {
+      amount: Number(amount.value),
+      sale_price: sale_price,
+      purchase_price: cost_price,
+    });
+  };
+
   return (
     <Container>
-      <Link href="/products/create">Añadir producto</Link>
+      <Container>
+        <h2>Ventas semanales</h2>
+        <Container
+          styles={{
+            width: "500px",
+            height: "200px",
+            backgroundColor: "#ccc",
+            borderRadius: "20px",
+            marginBottom: "20px",
+          }}
+        ></Container>
+      </Container>
+      <Form onSubmit={handlerOnSubmit}>
+        <h2>Cargar producto</h2>
+        <FlexContainer>
+          <Container styles={{ width: "150px" }}>
+            <InputNumber name="productCostPrice" inline required>
+              Precio de comprá
+            </InputNumber>
+          </Container>
+          <Container styles={{ width: "150px" }}>
+            <InputNumber name="productSalePrice" inline required>
+              Precio de venta
+            </InputNumber>
+          </Container>
+          <Container styles={{ width: "100px" }}>
+            <InputNumber defaultValue={0} inline name="amount" required>
+              Ingresó
+              {selectedProduct?.data() ? `${selectedProduct.data().units}` : ""}
+            </InputNumber>
+          </Container>
+        </FlexContainer>
+        <Button>Agregar carga</Button>
+      </Form>
     </Container>
   );
 };

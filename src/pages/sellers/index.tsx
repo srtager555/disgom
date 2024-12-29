dd458645b449061cad53b283bb72bc728ced8efa
Carlos && Ponce <srtager555@gmail.com>
2024-12-29 02:36:48 -0600

    layout added

--- src/pages/sellers/index.tsx
+++ src/pages/sellers/index.tsx
@@ -1,7 +1,13 @@
+import { SellersLayout } from "@/components/layouts/Sellers.layout";
 import { NextPageWithLayout } from "@/pages/_app";
+import { ReactElement } from "react";
 
 const Page: NextPageWithLayout = () => {
-  return "";
+  return "a";
+};
+
+Page.getLayout = function getLayout(page: ReactElement) {
+  return <SellersLayout>{page}</SellersLayout>;
 };
 
 export default Page;

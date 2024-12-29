aaec7ef49b20584f7e402b32ff992851c94873d7
Carlos && Ponce <srtager555@gmail.com>
2024-12-29 17:39:01 -0600

    SellerDoc type created

--- src/tools/sellers/create.ts
+++ src/tools/sellers/create.ts
@@ -1,4 +1,4 @@
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, Timestamp } from "firebase/firestore";
 import { Firestore } from "../firestore";
 import { SellersCollection } from "../firestore/CollectionTyping";
 
@@ -6,6 +6,10 @@ export type SellerType = {
   name: string;
   hasInventory: boolean;
 };
+
+export interface SellersDoc extends SellerType {
+  created_at: Timestamp;
+}
 
 export async function createSeller(name: string, hasInventory: boolean) {
   const db = Firestore();

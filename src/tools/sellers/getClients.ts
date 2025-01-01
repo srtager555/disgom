3b407d5730dbc0a2112aea17edfce7df7b072ae5
Carlos && Ponce <srtager555@gmail.com>
2024-12-31 19:28:45 -0600

    tool to get all client from a seller created

--- src/tools/sellers/getClients.ts
+++ src/tools/sellers/getClients.ts
@@ -1,11 +1,20 @@
-import { collection, doc, getDocs } from "firebase/firestore";
+import {
+  collection,
+  CollectionReference,
+  doc,
+  getDocs,
+} from "firebase/firestore";
 import { Firestore } from "../firestore";
 import { SellersCollection } from "../firestore/CollectionTyping";
+import { client } from "./createClient";
 
 export async function getClients(seller_id: string) {
   const db = Firestore();
   const sellerDoc = doc(db, SellersCollection.root, seller_id);
-  const coll = collection(sellerDoc, SellersCollection.clients);
+  const coll = collection(
+    sellerDoc,
+    SellersCollection.clients
+  ) as CollectionReference<client>;
 
   return await getDocs(coll);
 }

ff64f0524c5e7c5c279133f388abcbc333dc993a
Carlos && Ponce <srtager555@gmail.com>
2025-01-01 22:30:03 -0600

    prototype to show the amount divided by stock created

---
+++ src/components/pages/invoice/Product/Extra.tsx
@@ -0,0 +1,119 @@
+import { Column, ExtraPrices, Input } from ".";
+import { stockType } from "@/tools/products/addToStock";
+import {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
+
+interface ExtraProps {
+  hasInventory: boolean | undefined;
+  stock: stockType;
+  extra: ExtraPrices;
+}
+
+export function Extra({ stock, extra, hasInventory }: ExtraProps) {
+  const [amount, setAmount] = useState(extra.amount);
+  const [editAmountValue, setEditAmountValue] = useState(false);
+  const [purchaseValue, setPurchaseValue] = useState(0);
+  const [salePrice, setSalePrice] = useState(stock.sale_price || 0);
+  const [saleValue, setSaleValue] = useState(0);
+  const [profitValue, setProfitValue] = useState(0);
+  const [sellerPrice, setSellerPrice] = useState(stock.seller_profit);
+  const [sellerValue, setSellerValue] = useState(0);
+  const [sellerProfit, setSellerProfit] = useState(0);
+
+  function changeValue(
+    e: ChangeEvent<HTMLInputElement> | number,
+    setState: Dispatch<SetStateAction<number>>
+  ) {
+    const value = typeof e === "number" ? e : Number(e.target.value);
+    setState(value);
+  }
+
+  function updateValue() {
+    setEditAmountValue(true);
+  }
+
+  useEffect(() => {
+    changeValue(extra.amount, setAmount);
+    setEditAmountValue(false);
+  }, [extra.amount]);
+
+  useEffect(() => {
+    if (!stock) return;
+    setPurchaseValue(amount * stock.purchase_price);
+    setSaleValue(amount * salePrice);
+    setProfitValue((salePrice - stock.purchase_price) * amount);
+
+    setSellerValue(amount * sellerPrice);
+    setSellerProfit((sellerPrice - salePrice) * amount);
+  }, [amount, salePrice, sellerPrice, sellerValue, stock]);
+
+  return (
+    <>
+      <Column $gridColumn="1 / 4">
+        {stock.created_at.toDate().toLocaleDateString()}
+      </Column>
+      <Column $gridColumn="4 / 5">
+        <Input
+          onChange={(e) => {
+            changeValue(e, setAmount);
+          }}
+          onClick={updateValue}
+          type="number"
+          value={!editAmountValue ? extra.amount : undefined}
+          max={stock.amount}
+          min={0}
+        />
+      </Column>
+      <Column $gridColumn="5 / 6">{stock.purchase_price}</Column>
+      <Column $gridColumn="6 / 7" title={purchaseValue.toLocaleString()}>
+        {purchaseValue.toLocaleString()}
+      </Column>
+      <Column $gridColumn="7 / 8">
+        {stock.sale_price ? (
+          <Input
+            onChange={(e) => changeValue(e, setSalePrice)}
+            type="number"
+            min={stock.purchase_price}
+            defaultValue={salePrice || stock.sale_price}
+          />
+        ) : (
+          "~"
+        )}
+      </Column>
+      <Column $gridColumn="8 / 9" title={saleValue.toLocaleString()}>
+        {saleValue.toLocaleString()}
+      </Column>
+      <Column $gridColumn="9 / 10" title={profitValue.toLocaleString()}>
+        {profitValue.toLocaleString()}
+      </Column>
+      {hasInventory && (
+        <>
+          <Column $gridColumn="10 / 11">
+            {stock.seller_profit ? (
+              <Input
+                onChange={(e) => changeValue(e, setSellerPrice)}
+                type="number"
+                min={salePrice}
+                defaultValue={sellerPrice || stock.seller_profit}
+              />
+            ) : (
+              "~"
+            )}
+          </Column>
+          <Column $gridColumn="11 / 12" title={sellerValue.toLocaleString()}>
+            {sellerValue.toLocaleString()}
+          </Column>
+          <Column $gridColumn="12 / 13" title={sellerProfit.toLocaleString()}>
+            {sellerProfit.toLocaleString()}
+          </Column>
+        </>
+      )}
+      <Column $gridColumn="-1 / -2">x</Column>
+    </>
+  );
+}

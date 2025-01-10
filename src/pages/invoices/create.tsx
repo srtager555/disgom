77d496edc4b886d22b2d91ec2836c75086ac0fcd
Carlos && Ponce <srtager555@gmail.com>
2025-01-10 04:06:47 -0600

    function prototype to create a invoice created

--- src/pages/invoices/create.tsx
+++ src/pages/invoices/create.tsx
@@ -8,8 +8,12 @@ import { Result } from "@/components/pages/invoice/Result";
 import { SelectClient } from "@/components/pages/invoice/SelectClient";
 import { useGetSellers } from "@/hooks/sellers/getSellers";
 import { NextPageWithLayout } from "@/pages/_app";
+import { Button } from "@/styles/Form.styles";
 import { Container, FlexContainer } from "@/styles/index.styles";
+import { createInvoice, invoiceType } from "@/tools/invoices/createInvoice";
+import { addOutputs } from "@/tools/products/addOutputs";
 import { SellersDoc } from "@/tools/sellers/create";
+import { client } from "@/tools/sellers/createClient";
 import { QueryDocumentSnapshot } from "firebase/firestore";
 import {
   ChangeEvent,
@@ -47,9 +51,63 @@ const Page: NextPageWithLayout = () => {
   const [productsResults, setProductsResults] = useState<
     Record<string, productResult>
   >({});
+  const [client, setClient] = useState<QueryDocumentSnapshot<client> | null>(
+    null
+  );
+  const [isCredit, setIsCredit] = useState(false);
 
   function selectSeller(e: ChangeEvent<HTMLSelectElement>) {
     setSelectedSeller(e.target.value);
+  }
+
+  async function handlerCreateInvoice() {
+    if (!sellerDoc || !productsResults) return;
+
+    const productResultValues = Object.values(productsResults);
+    const totals = productResultValues.reduce(
+      (before, now) => {
+        return {
+          total_cost: before.total_cost + now.cost,
+          total_sold: before.total_sold + now.sold.total,
+          total_profit: before.total_profit + now.profit,
+        };
+      },
+      {
+        total_cost: 0,
+        total_sold: 0,
+        total_profit: 0,
+      }
+    );
+    // const productResultEntries = Object.entries(productsResults);
+    // productResultEntries.map(async (el) => addOutputs(el[0], el[1]));
+
+    const invoiceData: Omit<invoiceType, "created_at"> = {
+      seller_ref: sellerDoc?.ref,
+      client_ref: client?.ref || null,
+      inventory_ref: null,
+      products_outputs: [],
+      last_inventory_ref: null,
+      total_cost: {
+        normal: totals.total_cost,
+        withInventory: totals.total_cost,
+      },
+      total_sold: {
+        normal: totals.total_sold,
+        withInventory: totals.total_sold,
+      },
+      total_proft: {
+        normal: totals.total_profit,
+        withInventory: totals.total_profit,
+      },
+      credit: sellerDoc.data().hasInventory
+        ? null
+        : {
+            paid: isCredit,
+            paid_at: null,
+          },
+    };
+
+    await createInvoice(invoiceData);
   }
 
   // effect to get the selected seller doc
@@ -83,12 +141,32 @@ const Page: NextPageWithLayout = () => {
             }
           />
         </SellersSelect>
-        <SelectClient sellerDoc={sellerDoc} sellerData={sellerData} />
+        <SelectClient
+          sellerDoc={sellerDoc}
+          sellerData={sellerData}
+          setClient={setClient}
+        />
         <ProductList setProductsResults={setProductsResults} />
         <Result
           hasInventory={sellerDoc?.data().hasInventory}
           productsResults={productsResults}
         />
+        <Container styles={{ marginTop: "20px", marginBottom: "100px" }}>
+          <p>
+            <label>
+              <input
+                type="checkbox"
+                onChange={(e) => {
+                  setIsCredit(e.target.checked);
+                }}
+              />
+              <span style={{ display: "inline-block", marginLeft: "10px" }}>
+                ¿Esta factura será cancelada ahorá?
+              </span>
+            </label>
+          </p>
+          <Button onClick={handlerCreateInvoice}>Crear factura</Button>
+        </Container>
       </Container>
     </InvoiceContext.Provider>
   );

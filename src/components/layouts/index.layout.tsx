c344248a8278b005f613f23fe6d2bde9b5cd668e
Carlos && Ponce <srtager555@gmail.com>
2025-01-12 04:03:41 -0600

    context to listen the print event added

--- src/components/layouts/index.layout.tsx
+++ src/components/layouts/index.layout.tsx
@@ -1,13 +1,38 @@
 import { InitApp } from "@/firebase/InitApp";
 import { NavLayout } from "./nav.layout";
 import { Container } from "@/styles/index.styles";
+import { createContext, useEffect, useState } from "react";
+
+export const PrintContext = createContext(false);
 
 export function Layout({ children }: { children: children }) {
+  const [print, setPrint] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeprint", () => {
+      setPrint(true);
+    });
+    window.addEventListener("afterprint", () => {
+      setPrint(false);
+    });
+
+    return () => {
+      window.removeEventListener("beforeprint", () => {
+        setPrint(true);
+      });
+      window.removeEventListener("afterprint", () => {
+        setPrint(false);
+      });
+    };
+  }, []);
+
   return (
-    <InitApp>
-      <NavLayout>
-        <Container styles={{ padding: "0 2.5%" }}>{children}</Container>
-      </NavLayout>
-    </InitApp>
+    <PrintContext.Provider value={print}>
+      <InitApp>
+        <NavLayout>
+          <Container styles={{ padding: "0 2.5%" }}>{children}</Container>
+        </NavLayout>
+      </InitApp>
+    </PrintContext.Provider>
   );
 }

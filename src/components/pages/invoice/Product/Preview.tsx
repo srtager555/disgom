bd56da36bfdf9b121874ea5b01b6542582274d81
Carlos && Ponce <srtager555@gmail.com>
2025-01-11 23:11:33 -0600

    logic to show the product fade added

---
+++ src/components/pages/invoice/Product/Preview.tsx
@@ -0,0 +1,195 @@
+import { invoiceOwners, rawProduct } from "@/pages/invoices/preview";
+import { Column, ExtraButton, ProductName } from ".";
+import { ProductContainer } from "../ProductList";
+import { useEffect, useMemo, useState } from "react";
+import { Firestore } from "@/tools/firestore";
+import {
+  doc,
+  DocumentReference,
+  DocumentSnapshot,
+  getDoc,
+} from "firebase/firestore";
+import { ProductsCollection } from "@/tools/firestore/CollectionTyping";
+import { productDoc } from "@/tools/products/create";
+import { numberParser } from "@/tools/numberPaser";
+import { Icon } from "@/components/Icons";
+
+type props = {
+  owners: invoiceOwners;
+  product_id: string;
+  data: rawProduct;
+};
+
+export function ProductPreview({ data, owners, product_id }: props) {
+  const [product, setProduct] = useState<DocumentSnapshot<productDoc>>();
+  const productData = useMemo(() => product?.data(), [product]);
+  const sellerData = useMemo(() => owners.seller.data(), [owners.seller]);
+  const clientData = useMemo(() => owners.client?.data(), [owners.client]);
+
+  const [fold, setFold] = useState(false);
+
+  const totalAmounts = useMemo(
+    () =>
+      data.purchases_amounts.reduce(
+        (before, now) => {
+          return {
+            amount: before.amount + now.amount,
+            purchase_cost: before.purchase_cost + now.total,
+          };
+        },
+        {
+          amount: 0,
+          purchase_cost: 0,
+        }
+      ),
+    [data]
+  );
+  const costPrices = useMemo(() => {
+    return data.purchases_amounts.map((el) => el.price);
+  }, [data]);
+  const totalSales = useMemo(
+    () =>
+      data.sales_amounts.reduce(
+        (before, now) => {
+          return {
+            sale: before.sale + now.normal_total,
+            seller_sale: before.seller_sale + now.seller_total,
+          };
+        },
+        {
+          sale: 0,
+          seller_sale: 0,
+        }
+      ),
+    [data]
+  );
+  const normalSalePrices = useMemo(() => {
+    return data.sales_amounts.map((el) => el.normal_price);
+  }, [data]);
+  const sellerSalePrices = useMemo(() => {
+    return data.sales_amounts.map((el) => el.seller_price);
+  }, [data]);
+
+  // effect to get the product
+  useEffect(() => {
+    async function getProduct() {
+      const db = Firestore();
+      const prodRef = doc(
+        db,
+        ProductsCollection.root,
+        product_id
+      ) as DocumentReference<productDoc>;
+      const p = await getDoc(prodRef);
+      setProduct(p);
+    }
+
+    getProduct();
+  }, [product_id]);
+
+  return (
+    <ProductContainer
+      $hasInventory={sellerData?.hasInventory}
+      $withoutStock={1}
+    >
+      <Column gridColumn="1 / 4">
+        <ProductName>{productData?.name}</ProductName>
+      </Column>
+      <Column gridColumn="4 / 5">{totalAmounts.amount}</Column>
+      <Column gridColumn="5 / 6">
+        {costPrices.length > 1 ? "~" : numberParser(costPrices[0])}
+      </Column>
+      <Column
+        gridColumn="6 / 7"
+        title={numberParser(totalAmounts.purchase_cost)}
+      >
+        {numberParser(totalAmounts.purchase_cost)}
+      </Column>
+      <Column gridColumn="7 / 8">
+        {normalSalePrices.length > 1 ? "~" : numberParser(normalSalePrices[0])}
+      </Column>
+      <Column gridColumn="8 / 9" title={numberParser(totalSales.sale)}>
+        {numberParser(totalSales.sale)}
+      </Column>
+      <Column
+        gridColumn="9 / 10"
+        title={numberParser(totalSales.sale - totalAmounts.purchase_cost)}
+      >
+        {numberParser(totalSales.sale - totalAmounts.purchase_cost)}
+      </Column>
+      {sellerData?.hasInventory && (
+        <>
+          <Column gridColumn="10 / 11">
+            {sellerSalePrices.length > 1 ? "~" : sellerSalePrices[0]}
+          </Column>
+          <Column
+            gridColumn="11 / 12"
+            title={numberParser(totalSales.seller_sale)}
+          >
+            {numberParser(totalSales.seller_sale)}
+          </Column>
+          <Column
+            gridColumn="12 / 13"
+            title={numberParser(totalSales.seller_sale - totalSales.sale)}
+          >
+            {numberParser(totalSales.seller_sale - totalSales.sale)}
+          </Column>
+        </>
+      )}
+      <Column gridColumn="-1 / -2">
+        <ExtraButton onClick={() => setFold(!fold)}>
+          <Icon iconType={fold ? "fold" : "unfold"} />
+        </ExtraButton>
+      </Column>
+      {/*
+      <ProductContainer
+        $children
+        $hasInventory={hasInventory}
+        $withoutStock={stockAmount}
+        $fold={!fold}
+      >
+        <Column gridColumn="1 / -1" $removeBorder>
+          <b>Precios de la salida detallados</b>
+        </Column>
+        {currentStock &&
+          requestPricesValues.map((el, i) => (
+            <Price
+              key={i}
+              hasInventory={hasInventory}
+              stockInfo={currentStock}
+              priceRequestLength={requestPricesValues.length}
+              thePrice={salePrice}
+              theSellerPrice={sellerPrice}
+              priceRequest={el}
+              setSaleData={setPriceRequestDescription}
+              saleData={priceRequestDescription}
+              setRequestData={setRequestPricesValues}
+              index={i}
+            />
+          ))}
+        <ProductContainer
+          $children
+          $hasInventory={hasInventory}
+          $withoutStock={stockAmount}
+        >
+          <Column gridColumn="4 / 8" $removeBorder>
+            <Button onClick={addNewPriceRequest}>Agregar variaciones</Button>
+          </Column>
+        </ProductContainer>
+        <Column gridColumn="1 / -1" $removeBorder>
+          <b>Costos de la salida detallados</b>
+        </Column>
+        {stocks &&
+          costRequestsData.map((el, i) => (
+            <Cost
+              key={i}
+              outputRequest={el}
+              stockInfo={stocks[el.stockPosition]}
+              hasInventory={hasInventory}
+              setState={setCostValues}
+              index={i}
+            />
+          ))}
+      </ProductContainer> */}
+    </ProductContainer>
+  );
+}

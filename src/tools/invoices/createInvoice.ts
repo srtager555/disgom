c8a350b4f649ad8b73495d95cf97ddf669d51235
Carlos && Ponce <srtager555@gmail.com>
2025-01-15 07:03:10 -0600

    logic to add the last inventory added

--- src/tools/invoices/createInvoice.ts
+++ src/tools/invoices/createInvoice.ts
@@ -3,13 +3,22 @@ import {
   collection,
   CollectionReference,
   DocumentReference,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
   Timestamp,
 } from "firebase/firestore";
 import { Firestore } from "../firestore";
-import { InvoiceCollection } from "../firestore/CollectionTyping";
+import {
+  InvoiceCollection,
+  SellersCollection,
+} from "../firestore/CollectionTyping";
 import { SellersDoc } from "../sellers/create";
 import { client } from "../sellers/createClient";
 import { outputType } from "../products/addOutputs";
+import { bill } from "@/components/pages/invoice/Product/closing/Bills";
 
 export type invoiceType = {
   created_at: Timestamp;
@@ -30,6 +39,12 @@ export type invoiceType = {
     normal: number;
     withInventory: number;
   };
+  route: number | null;
+  bills: Array<bill> | null;
+  money: {
+    deposit: number;
+    cash: number;
+  } | null;
   credit: {
     paid: boolean;
     paid_at: Timestamp | null;
@@ -43,8 +58,20 @@ export async function createInvoice(data: Omit<invoiceType, "created_at">) {
     InvoiceCollection.root
   ) as CollectionReference<invoiceType>;
 
+  const sellerDoc = await getDoc(data.seller_ref);
+  let last_inventory = null;
+
+  if (sellerDoc.data()?.hasInventory) {
+    const coll = collection(sellerDoc.ref, SellersCollection.inventories.root);
+    const q = query(coll, orderBy("created_at", "desc"), limit(1));
+    const inventories = await getDocs(q);
+
+    last_inventory = inventories.docs[0];
+  }
+
   return await addDoc(coll, {
     created_at: Timestamp.fromDate(new Date()),
     ...data,
+    last_inventory_ref: last_inventory?.ref || null,
   });
 }

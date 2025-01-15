0015239a519b9cea19b313271b3aeac03ae52835
Carlos && Ponce <srtager555@gmail.com>
2025-01-15 02:50:18 -0600

    tools to get the client credits

---
+++ src/tools/sellers/credits/get.ts
@@ -0,0 +1,45 @@
+import {
+  collection,
+  CollectionReference,
+  DocumentReference,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  QueryDocumentSnapshot,
+  where,
+} from "firebase/firestore";
+import { SellersDoc } from "../create";
+import { SellersCollection } from "@/tools/firestore/CollectionTyping";
+import { clientCredit, credit } from "./create";
+
+export async function getCredits(
+  route: number,
+  seller_ref: DocumentReference<SellersDoc>
+) {
+  const creditColl = collection(
+    seller_ref,
+    SellersCollection.credits
+  ) as CollectionReference<clientCredit>;
+
+  const q = query(creditColl, where("route", "==", route));
+  const clients = await getDocs(q);
+
+  return clients;
+}
+
+export async function getClientCredits(
+  clientCreditDoc: QueryDocumentSnapshot<clientCredit>
+) {
+  const coll = collection(
+    clientCreditDoc.ref,
+    "credits"
+  ) as CollectionReference<credit>;
+  const q = query(coll, orderBy("created_at", "desc"), limit(1));
+
+  const credits = await getDocs(q);
+
+  if (credits.size > 0) {
+    return credits.docs[0];
+  } else return undefined;
+}

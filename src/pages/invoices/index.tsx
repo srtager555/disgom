cbb4a66627e9dd1efd906b4e0b0b724ff38435a0
Carlos && Ponce <srtager555@gmail.com>
2025-01-17 03:06:22 -0600

    logic to show invoice by added

--- src/pages/invoices/index.tsx
+++ src/pages/invoices/index.tsx
@@ -3,6 +3,7 @@ import {
   InvoiceContainer,
   InvoicePreview,
 } from "@/components/pages/invoice/InvoicePreview";
+import { Input } from "@/components/pages/invoice/Product";
 import { NextPageWithLayout } from "@/pages/_app";
 import { Container } from "@/styles/index.styles";
 import { Firestore } from "@/tools/firestore";
@@ -23,6 +24,7 @@ const Page: NextPageWithLayout = () => {
   const [docsInvoices, setDocsInvoices] = useState<
     QueryDocumentSnapshot<invoiceType>[]
   >([]);
+  const [date, setDate] = useState("");
 
   useEffect(() => {
     const db = Firestore();
@@ -31,24 +33,50 @@ const Page: NextPageWithLayout = () => {
       InvoiceCollection.root
     ) as CollectionReference<invoiceType>;
 
-    const date = new Date();
-    const today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+    const [year, month, day] = date.split("-").map(Number);
+
+    const dateee = new Date(
+      date != "" ? new Date(year, month - 1, day) : new Date()
+    );
+
+    const today = new Date(
+      dateee.getFullYear(),
+      dateee.getMonth(),
+      dateee.getDate()
+    );
+    const nextDay = new Date(today);
+    nextDay.setDate(nextDay.getDate() + 1);
 
     const timeInTimestamp = Timestamp.fromDate(today);
+    const timeInTimestampNextDay = Timestamp.fromDate(nextDay);
 
-    const q = query(coll, where("created_at", ">=", timeInTimestamp));
+    const q = query(
+      coll,
+      where("created_at", ">=", timeInTimestamp),
+      where("created_at", "<", timeInTimestampNextDay)
+    );
     const unsubcribe = onSnapshot(q, (snap) => {
       const invoices = snap.docs;
       setDocsInvoices(invoices);
     });
 
     return unsubcribe;
-  }, []);
+  }, [date]);
 
   return (
     <Container>
-      {docsInvoices.length === 0 ? (
+      <Container styles={{ display: "inline-block", marginBottom: "20px" }}>
+        <Input
+          type="date"
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+        />
+      </Container>
+      {docsInvoices.length === 0 && date === "" ? (
         <h2>No hay faturas para hoy</h2>
+      ) : docsInvoices.length === 0 && date != "" ? (
+        <h2>No se encontraron facturas para el {date}</h2>
       ) : (
         <InvoiceContainer>
           {docsInvoices.map((el, i) => {

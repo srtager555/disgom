2480dce79fb3cf4f9c4b89c59a2e42c5c2b3b210
Carlos && Ponce <srtager555@gmail.com>
2025-02-03 21:38:21 -0600

    Component to select the product tags

---
+++ src/components/pages/products/SelectTag.tsx
@@ -0,0 +1,35 @@
+import { Select } from "@/components/Inputs/select";
+import { useGetTags } from "@/hooks/products/getTags";
+import { FlexContainer } from "@/styles/index.styles";
+import { Dispatch, SetStateAction } from "react";
+
+type props = {
+  setTagSelected: Dispatch<SetStateAction<string>>;
+};
+
+export function SelectTag({ setTagSelected }: props) {
+  const tags = useGetTags();
+
+  return (
+    <FlexContainer styles={{ marginRight: "20px", alignItems: "center" }}>
+      <span style={{ marginRight: "10px", display: "inline-block" }}>
+        Filtrar por etiquetas
+      </span>
+      {tags && (
+        <Select
+          marginBottom="0px"
+          onChange={(e) => {
+            setTagSelected(e.target.value);
+          }}
+          options={[
+            { name: "Sin filtro", value: "", selected: true },
+            ...Object.values(tags).map((el) => ({
+              name: el.name,
+              value: el.name,
+            })),
+          ]}
+        />
+      )}
+    </FlexContainer>
+  );
+}

3a5daab71ba3760c3f80e29424ea62c48573c6ca
Carlos && Ponce <srtager555@gmail.com>
2025-02-11 12:53:37 -0600

    logic to get products with and without parent added

--- src/hooks/products/getProducts.ts
+++ src/hooks/products/getProducts.ts
@@ -9,10 +9,30 @@ import {
   orderBy,
   query,
   QueryDocumentSnapshot,
+  QueryFieldFilterConstraint,
+  QueryOrderByConstraint,
   QuerySnapshot,
   where,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
+
+function makeReference(
+  baseWheres: (QueryFieldFilterConstraint | QueryOrderByConstraint)[],
+  conditional: boolean,
+  conditionalWheres: (QueryFieldFilterConstraint | QueryOrderByConstraint)[]
+) {
+  const db = Firestore();
+  const coll = collection(
+    db,
+    ProductsCollection.root
+  ) as CollectionReference<productDoc>;
+
+  const wheres = baseWheres;
+
+  if (conditional) wheres.push(...conditionalWheres);
+
+  return query(coll, ...wheres);
+}
 
 export function useGetProducts(tag: string = "") {
   const [snap, setSnap] = useState<QuerySnapshot<productDoc>>();
@@ -20,33 +40,22 @@ export function useGetProducts(tag: string = "") {
     useState<QueryDocumentSnapshot<productDoc, DocumentData>[]>();
   const [docsDisabled, setDocsDisabled] =
     useState<QueryDocumentSnapshot<productDoc, DocumentData>[]>();
+  const [docsWithoutParent, setDocsWithoutParent] =
+    useState<QueryDocumentSnapshot<productDoc, DocumentData>[]>();
+  const [docsWithParent, setDocsWithParent] =
+    useState<QueryDocumentSnapshot<productDoc, DocumentData>[]>();
 
   // effect to the normal products
   useEffect(() => {
-    const db = Firestore();
-    const coll = collection(
-      db,
-      ProductsCollection.root
-    ) as CollectionReference<productDoc>;
-    let q;
+    const wheres = [
+      where("exclude", "!=", true),
+      where("disabled", "==", false),
+      orderBy("name"),
+    ];
+    const conditionalWheres = [where("tags", "array-contains", tag)];
+    const query = makeReference(wheres, tag != "", conditionalWheres);
 
-    if (tag != "")
-      q = query(
-        coll,
-        where("exclude", "!=", true),
-        where("disabled", "==", false),
-        where("tags", "array-contains", tag),
-        orderBy("name")
-      );
-    else
-      q = query(
-        coll,
-        where("exclude", "!=", true),
-        where("disabled", "==", false),
-        orderBy("name")
-      );
-
-    const unsubcribe = onSnapshot(q, (snap) => {
+    const unsubcribe = onSnapshot(query, (snap) => {
       setSnap(snap);
       setDocs(snap.docs);
     });
@@ -58,30 +67,15 @@ export function useGetProducts(tag: string = "") {
 
   // effect to the disabled products
   useEffect(() => {
-    const db = Firestore();
-    const coll = collection(
-      db,
-      ProductsCollection.root
-    ) as CollectionReference<productDoc>;
-    let q;
+    const wheres = [
+      where("exclude", "!=", true),
+      where("disabled", "==", true),
+      orderBy("name"),
+    ];
+    const conditionalWheres = [where("tags", "array-contains", tag)];
+    const query = makeReference(wheres, tag != "", conditionalWheres);
 
-    if (tag != "")
-      q = query(
-        coll,
-        where("exclude", "!=", true),
-        where("disabled", "==", true),
-        where("tags", "array-contains", tag),
-        orderBy("name")
-      );
-    else
-      q = query(
-        coll,
-        where("exclude", "!=", true),
-        where("disabled", "==", true),
-        orderBy("name")
-      );
-
-    const unsubcribe = onSnapshot(q, (snap) => {
+    const unsubcribe = onSnapshot(query, (snap) => {
       setDocsDisabled(snap.docs);
     });
 
@@ -90,5 +84,43 @@ export function useGetProducts(tag: string = "") {
     };
   }, [tag]);
 
-  return { snap, docs, docsDisabled };
+  // effect to the return only the products withOutParent
+  useEffect(() => {
+    const wheres = [
+      where("exclude", "==", false),
+      where("disabled", "==", false),
+      where("product_parent", "==", null),
+      orderBy("name"),
+    ];
+    const query = makeReference(wheres, false, []);
+
+    const unsubcribe = onSnapshot(query, (snap) => {
+      setDocsWithoutParent(snap.docs);
+    });
+
+    return function () {
+      unsubcribe();
+    };
+  }, []);
+
+  // effect to return only the products withParent
+  useEffect(() => {
+    const wheres = [
+      where("exclude", "==", false),
+      where("disabled", "==", false),
+      where("product_parent", "!=", null),
+      orderBy("name"),
+    ];
+    const query = makeReference(wheres, false, []);
+
+    const unsubcribe = onSnapshot(query, (snap) => {
+      setDocsWithParent(snap.docs);
+    });
+
+    return function () {
+      unsubcribe();
+    };
+  }, []);
+
+  return { snap, docs, docsDisabled, docsWithParent, docsWithoutParent };
 }

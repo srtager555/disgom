26b9dc5f5da7e072636e18183ae464a0f2a9a50e
Carlos && Ponce <srtager555@gmail.com>
2025-02-11 17:06:09 -0600

    A created a new component to manage the stock in another file

---
+++ src/components/pages/invoice/Products/Stock.tsx
@@ -0,0 +1,96 @@
+import { useGetProduct } from "@/hooks/products/getProduct";
+import { globalCSSVars } from "@/styles/colors";
+import { Button } from "@/styles/Form.styles";
+import { Container, FlexContainer } from "@/styles/index.styles";
+import { stockType } from "@/tools/products/addToStock";
+import { Dispatch, SetStateAction } from "react";
+import styled, { css } from "styled-components";
+
+const StockMapContainer = styled(Container)`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+  /* padding: 10px;
+  border: solid 2px ${globalCSSVars["--foreground"]}; */
+  border-radius: 20px;
+  margin-bottom: 20px;
+`;
+
+const StockButton = styled(Button)<{ $selected: boolean }>`
+  text-align: start;
+  padding: 5px;
+  width: 100%;
+  background-color: ${(props) =>
+    props.$selected
+      ? globalCSSVars["--selected"]
+      : globalCSSVars["--background"]};
+  ${(props) =>
+    props.$selected &&
+    css`
+      color: #fff;
+      transform: scale(1.04);
+    `}
+
+  &:hover {
+    transform: scale(1.02);
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+`;
+
+type props = {
+  stock: stockType[];
+  entryToEdit: stockType | undefined;
+  setEntryToEdit: Dispatch<SetStateAction<stockType | undefined>>;
+};
+
+export function ProductStock({ stock, entryToEdit, setEntryToEdit }: props) {
+  const product = useGetProduct();
+
+  // function to select and unselect a entry
+  function handlerSelectEntry(stock: stockType) {
+    if (entryToEdit === stock) setEntryToEdit(undefined);
+    else setEntryToEdit(stock);
+  }
+
+  return (
+    <>
+      <h3>Existencias</h3>
+      {stock.length > 0 && <p>Para editar una entrada seleccionela</p>}
+      <StockMapContainer>
+        {stock.length === 0 ? (
+          <p>No hay existencia de este producto</p>
+        ) : (
+          <>
+            {stock.map((_, i) => {
+              return (
+                <Container key={i} styles={{ width: "100%" }}>
+                  <StockButton
+                    $selected={_ === entryToEdit}
+                    onClick={() => handlerSelectEntry(_)}
+                  >
+                    {_.created_at.toDate().toLocaleDateString()} - hay{" "}
+                    {_.amount} {product.data?.units}
+                    <FlexContainer>
+                      <Container styles={{ marginRight: "10px" }}>
+                        Costo {_.purchase_price} -
+                      </Container>
+                      <Container styles={{ marginRight: "10px" }}>
+                        Precio {_.sale_price} -
+                      </Container>
+                      <Container>Vendedor {_.seller_commission}</Container>
+                    </FlexContainer>
+                  </StockButton>
+                </Container>
+              );
+            })}
+          </>
+        )}
+      </StockMapContainer>
+    </>
+  );
+}

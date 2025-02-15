05506937ba695c9b5d1fada2899a1e40e62f28d8
Carlos && Ponce <srtager555@gmail.com>
2025-02-15 12:09:42 -0600

    custom hook to get the invoice in real time

---
+++ src/hooks/invoice/getInvoiceByQueryOnSnapshot.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import {
+  onSnapshot,
+  doc,
+  QueryDocumentSnapshot,
+  DocumentReference,
+} from "firebase/firestore";
+import { Firestore } from "@/tools/firestore";
+import { invoiceType } from "@/tools/invoices/createInvoice";
+import useQueryParams from "../getQueryParams";
+import { InvoiceCollection } from "@/tools/firestore/CollectionTyping";
+
+export function useGetInvoiceByQueryOnSnapshot() {
+  const { id } = useQueryParams();
+  const [invoice, setInvoice] = useState<
+    QueryDocumentSnapshot<invoiceType> | undefined
+  >();
+
+  useEffect(() => {
+    if (!id) return;
+
+    const ref = doc(
+      Firestore(),
+      InvoiceCollection.root,
+      id
+    ) as DocumentReference<invoiceType>;
+
+    const unsubscribe = onSnapshot(ref, (snap) => {
+      if (snap.exists()) {
+        setInvoice(snap);
+      } else {
+        setInvoice(undefined);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [id]);
+
+  return invoice;
+}

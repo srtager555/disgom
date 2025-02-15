fbfce0a3e3f340b78ef799777914d8d63e61b53e
Carlos && Ponce <srtager555@gmail.com>
2025-02-15 12:13:47 -0600

    Memo and AddOuput component added

--- src/components/pages/invoice/manage/products/Product.tsx
+++ src/components/pages/invoice/manage/products/Product.tsx
@@ -1,11 +1,11 @@
 import { productDoc } from "@/tools/products/create";
 import { QueryDocumentSnapshot } from "firebase/firestore";
 import { ProductContainer } from "../../ProductList";
-import { useMemo } from "react";
-import { Column } from "../../Product";
+import { memo, useMemo } from "react";
+import { Column, Input } from "../../Product";
 import styled from "styled-components";
 import { SellersDoc } from "@/tools/sellers/create";
-import { useGetInventoryByProduct } from "@/hooks/invoice/getInventoryByProduct";
+import { AddOutput } from "./AddOutput";
 
 const GrabButton = styled.button`
   display: inline-block;
@@ -29,25 +29,49 @@ type props = {
   selectedSeller: QueryDocumentSnapshot<SellersDoc> | undefined;
 };
 
+export const MemoProduct = memo(Product, (prev, next) => {
+  const prevDocID = prev.doc.id;
+  const nextDocID = next.doc.id;
+
+  const prevSellerID = prev.selectedSeller?.id;
+  const nextSellerID = next.selectedSeller?.id;
+
+  if (prevDocID !== nextDocID) return false;
+  if (prevSellerID !== nextSellerID) return false;
+
+  return true;
+});
+
 export function Product({ doc, selectedSeller }: props) {
   const data = useMemo(() => doc.data(), [doc]);
+  const currentStock = data.stock.reduce((acc, stock) => {
+    return acc + stock.amount;
+  }, 0);
   const selectedSellerData = useMemo(
     () => selectedSeller?.data(),
     [selectedSeller]
   );
-  const inventory = useGetInventoryByProduct();
 
   return (
     <ProductContainer
       $hide={false}
       $hasInventory={selectedSellerData?.hasInventory}
       $withoutStock={1}
+      $after={`${currentStock} / ${data.stock.length}`}
     >
       <Column>
         <GrabButton>-</GrabButton>
       </Column>
       <Column gridColumn="2 / 5">{data.name}</Column>
       <Column>0</Column>
+      <AddOutput
+        productDoc={doc}
+        currentStock={currentStock}
+        stocks={data.stock}
+      />
+      <Column>
+        <Input type="number" />
+      </Column>
     </ProductContainer>
   );
 }

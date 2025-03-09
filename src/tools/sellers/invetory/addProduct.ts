7292583326020a03045b9a43ce8266bbeb3c4a13
Carlos && Ponce <srtager555@gmail.com>
2025-03-09 11:05:31 -0600

    small changes to improve the dev life

--- src/tools/sellers/invetory/addProduct.ts
+++ src/tools/sellers/invetory/addProduct.ts
@@ -7,25 +7,20 @@ import {
 import { inventory } from "./create";
 import { SellersCollection } from "@/tools/firestore/CollectionTyping";
 import { productDoc } from "@/tools/products/create";
+import { outputType } from "@/tools/products/addOutputs";
 
-export type inventory_product_data = {
-  amount: number;
-  purchase_price: number;
-  product_ref: DocumentReference<productDoc>;
-};
-
-export interface inventoryProductDoc extends inventory_product_data {
-  created_at: Timestamp;
+export interface inventory_output extends outputType {
+  inventory_ref: DocumentReference<inventory>;
 }
 
 export async function addInventoryProduct(
   inventory_ref: DocumentReference<inventory>,
-  data: inventory_product_data
+  data: inventory_output
 ) {
   const coll = collection(
     inventory_ref,
     SellersCollection.inventories.products
   );
 
-  return await addDoc(coll, { created_at: new Date(), ...data });
+  return await addDoc(coll, data);
 }

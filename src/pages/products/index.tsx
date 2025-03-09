e3165e6b5c9c9483c01103ef426b6ab6b672f7eb
Carlos && Ponce <srtager555@gmail.com>
2025-03-09 14:18:35 -0600

    File name changed

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,127 +1,28 @@
-import SalesComparisonChart from "@/components/chart";
 import {
   ProductContext,
   ProductsLayout,
 } from "@/components/layouts/Products.layout";
-import { FormToAddStock } from "@/components/pages/invoice/Products/FormToAddStock";
-import { ProductStock } from "@/components/pages/invoice/Products/Stock";
 import { useGetProduct } from "@/hooks/products/getProduct";
-import { NextPageWithLayout } from "@/pages/_app";
-import { Container } from "@/styles/index.styles";
-import { stockType } from "@/tools/products/addToStock";
-import { ReactElement, useContext, useEffect, useState } from "react";
-import styled, { css } from "styled-components";
-
-const FadeContainer = styled(Container)<{ $fade: boolean }>`
-  &:after {
-    content: "";
-    ${(props) =>
-      !props.$fade &&
-      css`
-        display: none;
-      `}
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    cursor: not-allowed;
-  }
-  ${(props) =>
-    props.$fade &&
-    css`
-      opacity: 0.3;
-    `}
-`;
-
-const MainContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(8, 1fr);
-  grid-auto-rows: 22px;
-  gap: 10px;
-`;
-
-const ChartContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  grid-column: 1 / 6;
-  grid-row: 1 / 8;
-`;
-
-// const Chart = styled.div`
-//   flex: 1;
-//   background-color: #ccc;
-//   border-radius: 20px;
-// `;
-
-const StockContainer = styled.div`
-  grid-column: 6 / 9;
-  grid-row-start: 1;
-`;
-
-const FormContainer = styled.div`
-  display: grid;
-  grid-column: 1 / 6;
-  grid-row: 9 / 15;
-`;
+import { useContext, ReactElement } from "react";
+import { NextPageWithLayout } from "../_app";
 
 const Page: NextPageWithLayout = () => {
   const { selectedProduct } = useContext(ProductContext);
   const product = useGetProduct();
 
-  const [entryToEdit, setEntryToEdit] = useState<stockType | undefined>();
-  const [stock, setStock] = useState<stockType[]>([]);
-
-  // effect to sort the product stock by date
   useEffect(() => {
-    const s = product.data?.stock;
-    if (!s || s?.length === 0) {
-      setStock([]);
-      return;
-    }
+    if (productsContext?.setShowProductsList)
+      productsContext?.setShowProductsList(false);
+    if (productsContext?.setHideBorder) productsContext?.setHideBorder(true);
 
-    const stock = s.sort((a, b) => {
-      return b.created_at.seconds - a.created_at.seconds;
-    });
+    return () => {
+      if (productsContext?.setShowProductsList)
+        productsContext?.setShowProductsList(true);
+      if (productsContext?.setHideBorder) productsContext?.setHideBorder(false);
+    };
+  }, [productsContext]);
 
-    setStock(stock);
-  }, [product.data?.stock]);
-
-  useEffect(() => {
-    setEntryToEdit(undefined);
-  }, [selectedProduct]);
-
-  return (
-    <Container>
-      <p style={{ fontStyle: "italic" }}>
-        Para ver información de un producto seleccionelo en la lista de
-        productos
-      </p>
-      <FadeContainer $fade={selectedProduct ? false : true}>
-        <h1>{selectedProduct?.data().name}</h1>
-        <MainContainer>
-          <ChartContainer>
-            <h3>Ventas semanales</h3>
-            <SalesComparisonChart />
-          </ChartContainer>
-          <StockContainer>
-            <ProductStock
-              stock={stock}
-              entryToEdit={entryToEdit}
-              setEntryToEdit={setEntryToEdit}
-            />
-          </StockContainer>
-          <FormContainer>
-            <FormToAddStock
-              stock={stock}
-              entryToEdit={entryToEdit}
-              setEntryToEdit={setEntryToEdit}
-            />
-          </FormContainer>
-        </MainContainer>
-      </FadeContainer>
-    </Container>
-  );
+  return <>Descripcion general</>;
 };
 
 Page.getLayout = function getLayout(Page: ReactElement) {

c9e93c1e97d7c99b895145d8e4df3d076a5d58c4
Carlos && Ponce <srtager555@gmail.com>
2025-03-22 22:53:21 -0600

    logic to save the devolution added

--- src/components/pages/invoice/manage/products/Devolution.tsx
+++ src/components/pages/invoice/manage/products/Devolution.tsx
@@ -1,34 +1,48 @@
 import { Dispatch, memo, SetStateAction, useEffect, useState } from "react";
 import { Column, Input } from "../../Product";
 import { useDebounce } from "@/hooks/debounce";
-import { outputType } from "@/tools/products/addOutputs";
-import { useGetProductOutputByID } from "@/hooks/invoice/getProductOutputsByID";
 import { DocumentSnapshot } from "firebase/firestore";
 import { productDoc } from "@/tools/products/create";
-import { isEqual } from "lodash";
-import { createStockFromOutputType } from "@/tools/products/ManageSaves";
+import { useGetCurrentDevolutionByProduct } from "@/hooks/invoice/getCurrentDevolution";
 
 type devolutionBase = {
   productDoc: DocumentSnapshot<productDoc>;
   setDevolutionAmount: Dispatch<SetStateAction<number>>;
   setHumanAmountChanged: Dispatch<SetStateAction<boolean>>;
-  totalOutputs: DocumentSnapshot<outputType>[];
+  // totalOutputs: DocumentSnapshot<outputType>[];
   sellerHasInventory: boolean | undefined;
+  currentDevolution: number;
 };
 
-export const Devolution = (props: Omit<devolutionBase, "totalOutputs">) => {
-  const outputs = useGetProductOutputByID(props.productDoc.id);
-  const totalOutputs = [...outputs];
+export const Devolution = (
+  props: Omit<devolutionBase, "totalOutputs" | "currentDevolution">
+) => {
+  const currentInventory = useGetCurrentDevolutionByProduct(
+    props.productDoc.id
+  );
+  // const outputs = useGetProductOutputByID(props.productDoc.id);
+  // const totalOutputs = [...outputs];
 
-  return <DevolutionMemo {...props} totalOutputs={totalOutputs} />;
+  useEffect(() => {
+    console.log(currentInventory);
+  }, [currentInventory]);
+
+  return (
+    <DevolutionMemo
+      {...props}
+      currentDevolution={currentInventory.amount}
+      //  totalOutputs={totalOutputs}
+    />
+  );
 };
 
 const DevolutionMemo = memo(DevolutionBase, (prev, next) => {
   if (prev.productDoc.id !== next.productDoc.id) return false;
   if (prev.setDevolutionAmount !== next.setDevolutionAmount) return false;
   if (prev.setHumanAmountChanged !== next.setHumanAmountChanged) return false;
-  if (!isEqual(prev.totalOutputs, next.totalOutputs)) return false;
+  // if (!isEqual(prev.totalOutputs, next.totalOutputs)) return false;
   if (prev.sellerHasInventory !== next.sellerHasInventory) return false;
+  if (prev.currentDevolution !== next.currentDevolution) return false;
 
   return true;
 });
@@ -36,8 +50,9 @@ const DevolutionMemo = memo(DevolutionBase, (prev, next) => {
 function DevolutionBase({
   setDevolutionAmount,
   setHumanAmountChanged,
-  totalOutputs,
+  // totalOutputs,
   sellerHasInventory,
+  currentDevolution,
 }: devolutionBase) {
   const [devo, setDevo] = useState(0);
   const devoDebounce = useDebounce(devo);
@@ -48,13 +63,17 @@ function DevolutionBase({
   }, [devoDebounce, setDevolutionAmount]);
 
   useEffect(() => {
-    const outputsParsedToStockType = totalOutputs.map((output) => {
-      const data = output.data() as outputType;
+    if (devo != currentDevolution) setDevo(currentDevolution);
+  }, [currentDevolution]);
 
-      const parsed = createStockFromOutputType(data);
-      return parsed;
-    });
-  }, []);
+  // useEffect(() => {
+  //   const outputsParsedToStockType = totalOutputs.map((output) => {
+  //     const data = output.data() as outputType;
+
+  //     const parsed = createStockFromOutputType(data);
+  //     return parsed;
+  //   });
+  // }, []);
 
   if (sellerHasInventory) {
     return (

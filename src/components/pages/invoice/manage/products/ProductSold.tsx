c5157ce4298f667dd9dd3104df2b30e04df52ea7
Carlos && Ponce <srtager555@gmail.com>
2025-03-23 19:57:06 -0600

    logic to save the devolution moved to another component

--- src/components/pages/invoice/manage/products/ProductSold.tsx
+++ src/components/pages/invoice/manage/products/ProductSold.tsx
@@ -1,34 +1,25 @@
 import { numberParser } from "@/tools/numberPaser";
 import { Column } from "../../Product";
 import { Dispatch, memo, SetStateAction, useEffect, useState } from "react";
-import {
-  amountListener,
-  createStockFromOutputType,
-} from "@/tools/products/ManageSaves";
-import { useGetProductOutputByID } from "@/hooks/invoice/getProductOutputsByID";
 import { addOutputs, outputType } from "@/tools/products/addOutputs";
 import {
   collection,
   CollectionReference,
   DocumentSnapshot,
   QueryDocumentSnapshot,
-  updateDoc,
 } from "firebase/firestore";
 import { productDoc } from "@/tools/products/create";
 import { isEqual } from "lodash";
 import { useGetInvoiceByQuery } from "@/hooks/invoice/getInvoiceByQuery";
-import {
-  addInventoryProduct,
-  inventory_output,
-} from "@/tools/sellers/invetory/addProduct";
-import { createInventory } from "@/tools/sellers/invetory/create";
 import { SellersDoc } from "@/tools/sellers/create";
+import { rawOutput } from "./AddOutput";
 
 type props = {
   product_doc: QueryDocumentSnapshot<productDoc>;
-  outputs: DocumentSnapshot<outputType>[];
+  // outputs: DocumentSnapshot<outputType>[];
   customPrice: number | undefined;
   humanAmountChanged: boolean;
+  remainStock: rawOutput[];
   outputsAmount: number;
   inventoryAmount: number;
   devolutionAmount: number;
@@ -39,9 +30,14 @@ type props = {
 };
 
 export const ProductSold = (props: Omit<props, "outputs">) => {
-  const outputs = useGetProductOutputByID(props.product_doc.id);
+  // const outputs = useGetProductOutputByID(props.product_doc.id);
 
-  return <MemoProductSold {...props} outputs={outputs} />;
+  return (
+    <MemoProductSold
+      {...props}
+      // outputs={outputs}
+    />
+  );
 };
 
 export const MemoProductSold = memo(ProductSoldBase, (prev, next) => {
@@ -52,14 +48,15 @@ export const MemoProductSold = memo(ProductSoldBase, (prev, next) => {
   if (!isEqual(prev.product_doc.id, next.product_doc.id)) return false;
   if (prev.customPrice != next.customPrice) return false;
   if (prev.humanAmountChanged != next.humanAmountChanged) return false;
-  if (!isEqual(prev.outputs, next.outputs)) return false;
+  if (!isEqual(prev.remainStock, next.remainStock)) return false;
+  // if (!isEqual(prev.outputs, next.outputs)) return false;
 
   return true;
 });
 
 export function ProductSoldBase({
-  outputs = [],
   customPrice,
+  remainStock,
   product_doc,
   outputsAmount,
   inventoryAmount,
@@ -67,7 +64,6 @@ export function ProductSoldBase({
   setAmount,
   setWarn,
   sellerHasInventory,
-  seletedSeller,
   humanAmountChanged,
 }: props) {
   const [total, setTotal] = useState(0);
@@ -88,57 +84,20 @@ export function ProductSoldBase({
       if (!invoiceDoc) return;
       if (!humanAmountChanged) return;
 
-      const allOutputs = outputs.concat(inventory_outputs);
-
-      const stock = allOutputs.map((el) =>
-        createStockFromOutputType(el.data() as outputType)
-      );
-
-      const outputsWorked = amountListener(
-        total,
-        stock,
-        product_doc,
-        customPrice
-      );
-
       const coll = collection(
         invoiceDoc.ref,
         "outputs_sold"
       ) as CollectionReference<outputType>;
+
       // outputs totalSold
-      const xxx = await addOutputs(
+      await addOutputs(
         invoiceDoc,
         product_doc,
-        outputsWorked.outputsToCreate,
+        remainStock,
         "outputs_sold",
         coll
         // true
       );
-
-      if (!seletedSeller) return;
-
-      const inventoryRef = await createInventory(
-        invoiceDoc.ref,
-        seletedSeller?.ref
-      );
-
-      const newInventory = outputsWorked.remainingStocks.map(
-        async (el) =>
-          await addInventoryProduct(inventoryRef, {
-            ...el,
-            inventory_ref: inventoryRef,
-          } as inventory_output)
-      );
-
-      await Promise.all(newInventory);
-
-      await updateDoc(invoiceDoc.ref, {
-        devolution: {
-          amount: devolutionAmount,
-          inventory_ref: inventoryRef,
-        },
-      });
-      console.log(xxx);
     }
 
     saveOutputsSolds();

fb823d3e2af21939c9c5d7683b2b1a7dcaa88c34
Carlos && Ponce <srtager555@gmail.com>
2025-04-06 13:44:32 -0600

    issue about re-update the stock fixed

--- src/tools/products/restaOutputs.ts
+++ src/tools/products/restaOutputs.ts
@@ -1,16 +1,13 @@
 import {
   DocumentReference,
   DocumentSnapshot,
-  QueryDocumentSnapshot,
   getDoc,
   updateDoc,
   Timestamp,
 } from "firebase/firestore";
 import { productDoc } from "./create";
 import { outputType } from "./addOutputs";
-import { rawOutput } from "@/components/pages/invoice/manage/products/AddOutput";
 import { disableOutput } from "./disableOutput";
-import { returnStock } from "./returnStock";
 import {
   createStockFromOutputType,
   amountListener,
@@ -20,7 +17,7 @@ import { invoiceType } from "@/tools/invoices/createInvoice";
 
 export async function restaOutputs(
   invoice: DocumentSnapshot<invoiceType>,
-  productDoc: QueryDocumentSnapshot<productDoc>,
+  productDoc: DocumentSnapshot<productDoc>,
   amount: number,
   currentAmount: number,
   customPrice?: number
@@ -58,18 +55,12 @@ export async function restaOutputs(
     )
   );
 
-  // 7. Devolver el stock usando returnStock
-  await Promise.all(
-    outputsToCreate.map((output: rawOutput) =>
-      returnStock(productDoc.ref, output)
-    )
-  );
-
-  // 8. Consolidar los stocks basados en entry_ref y precio de venta
+  // return;
+  // 7. Consolidar los stocks basados en entry_ref y precio de venta
   const currentProductStock = productDoc.data()?.stock || [];
   const consolidatedStocks = [...currentProductStock];
 
-  for (const newStock of remainingStocks) {
+  for (const newStock of outputsToCreate) {
     const existingStockIndex = consolidatedStocks.findIndex(
       (stock) =>
         stock.entry_ref.path === newStock.entry_ref.path &&
@@ -93,12 +84,12 @@ export async function restaOutputs(
     }
   }
 
-  // 9. Actualizar el stock del producto con los stocks consolidados
+  // 8. Actualizar el stock del producto con los stocks consolidados
   await updateDoc(productDoc.ref, {
     stock: consolidatedStocks,
   });
 
-  // 10. Guardar los nuevos outputs
+  // 9. Guardar los nuevos outputs
   await saveNewOutputs(invoice, productDoc, remainingStocks);
 
   console.log("Proceso de resta completado");

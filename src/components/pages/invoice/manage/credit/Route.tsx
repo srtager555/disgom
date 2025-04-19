3e7cebed861517f202d40656db8eb187d1a6f36b
Carlos && Ponce <srtager555@gmail.com>
2025-04-18 18:40:47 -0600

    some improves in the code added

--- src/components/pages/invoice/manage/credit/Route.tsx
+++ src/components/pages/invoice/manage/credit/Route.tsx
@@ -6,28 +6,35 @@ import { useMemo } from "react";
 
 export function Route() {
   const { invoice } = useInvoice();
-  const r = [
-    { name: "Seleccionar", value: "0", disabled: true, selected: true },
-    { name: "Ruta 1", value: "1" },
-    { name: "Ruta 2", value: "2" },
-    { name: "Ruta 3", value: "3" },
-    { name: "Ruta 4", value: "4" },
-    { name: "Ruta 5", value: "5" },
-    { name: "Ruta 6", value: "6" },
-  ];
   const routes = useMemo(() => {
     const router = invoice?.data()?.route;
+    const r = [
+      { name: "Seleccionar", value: "0", disabled: true },
+      { name: "Ruta 1", value: "1" },
+      { name: "Ruta 2", value: "2" },
+      { name: "Ruta 3", value: "3" },
+      { name: "Ruta 4", value: "4" },
+      { name: "Ruta 5", value: "5" },
+      { name: "Ruta 6", value: "6" },
+    ];
 
-    return r.map((r) => ({
-      ...r,
-      selected: Number(r.value) == router,
-    }));
+    return r.map((r) => {
+      return {
+        ...r,
+        selected: Number(r.value) == Number(router),
+      };
+    });
   }, [invoice]);
 
   const handlerUpdateRoute = async (
     e: React.ChangeEvent<HTMLSelectElement>
   ) => {
     if (!invoice) return;
+    if (!e.target.value) return;
+
+    const currentRouter = invoice.data().route;
+    if (Number(currentRouter) == Number(e.target.value)) return;
+
     await updateDoc(invoice.ref, {
       route: Number(e.target.value),
     });

a984faf5126a00b6e38b1970f695a5b74b291905
Carlos && Ponce <srtager555@gmail.com>
2025-04-19 22:59:09 -0600

    conditional added to avoid re-saves

---
+++ src/components/pages/invoice/manage/Closing/Bills.tsx
@@ -0,0 +1,62 @@
+import { GridContainer } from "@/styles/index.styles";
+import { numberParser } from "@/tools/numberPaser";
+import { Column, Input } from "../../Product";
+import { TotalResults } from "@/hooks/useProductResults";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { useDebounce } from "@/hooks/debounce";
+import { useInvoice } from "@/contexts/InvoiceContext";
+import { updateDoc } from "firebase/firestore";
+
+const gridTemplateColumns = "repeat(4, 100px)";
+
+type props = {
+  totals: TotalResults;
+  setBillsAmount: Dispatch<SetStateAction<number>>;
+};
+
+export function Bills({ totals, setBillsAmount }: props) {
+  const { invoice } = useInvoice();
+  const [currentBillsAmount, setCurrentBillsAmount] = useState(0);
+  const billsDebounce = useDebounce(currentBillsAmount);
+  const billsGetted = useRef(false);
+
+  useEffect(() => {
+    if (!invoice || billsGetted.current) return;
+    billsGetted.current = true;
+
+    setCurrentBillsAmount(invoice.data().bills);
+    setBillsAmount(invoice.data().bills);
+  }, [invoice]);
+
+  useEffect(() => {
+    async function saveBills() {
+      if (!invoice) return;
+      const dbBills = invoice.data().bills;
+      if (dbBills === billsDebounce) return;
+
+      await updateDoc(invoice.ref, {
+        bills: billsDebounce ?? 0,
+      });
+    }
+
+    saveBills();
+  }, [billsDebounce]);
+
+  return (
+    <GridContainer $gridTemplateColumns={gridTemplateColumns}>
+      <Column>Gastos</Column>
+      <Column>
+        <Input
+          value={currentBillsAmount}
+          onChange={(e) => {
+            const amount = Number(e.target.value);
+            setBillsAmount(amount);
+            setCurrentBillsAmount(amount);
+          }}
+        />
+      </Column>
+      <Column>Empresa</Column>
+      <Column>{numberParser(totals.totalProfit)}</Column>
+    </GridContainer>
+  );
+}

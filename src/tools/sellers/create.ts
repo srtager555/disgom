712dfdac17fc392ee2690e1ae815ba549cceeba7
Carlos && Ponce <srtager555@gmail.com>
2025-05-03 02:08:07 -0600

    new fields added to create special sellers

--- src/tools/sellers/create.ts
+++ src/tools/sellers/create.ts
@@ -1,26 +1,40 @@
 import { addDoc, collection, Timestamp } from "firebase/firestore";
 import { Firestore } from "../firestore";
 import { SellersCollection } from "../firestore/CollectionTyping";
+import { CollectionReference } from "firebase/firestore";
+import { invoiceType } from "../invoices/createInvoice";
 
 export type SellerType = {
   name: string;
   hasInventory: boolean;
   disabled: boolean;
+  exclude: boolean;
+  invoice_type: invoiceType["invoice_type"];
 };
 
 export interface SellersDoc extends SellerType {
   created_at: Timestamp;
 }
 
-export async function createSeller(name: string, hasInventory: boolean) {
+export async function createSeller(
+  name: string,
+  hasInventory: boolean,
+  exclude: boolean = false,
+  invoice_type: invoiceType["invoice_type"] = "normal"
+) {
   const db = Firestore();
 
-  const coll = collection(db, SellersCollection.root);
+  const coll = collection(
+    db,
+    SellersCollection.root
+  ) as CollectionReference<SellersDoc>;
 
-  await addDoc(coll, {
+  return await addDoc(coll, {
     name,
     hasInventory,
-    created_at: new Date(),
+    created_at: Timestamp.now(),
     disabled: false,
+    exclude,
+    invoice_type,
   });
 }

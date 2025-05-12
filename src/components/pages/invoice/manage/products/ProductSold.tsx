2672041f902dbf8606fd64ae754052d6f823e8ec
Carlos && Ponce <srtager555@gmail.com>
2025-05-11 19:20:32 -0600

    issue about incorrect try catch added

--- src/components/pages/invoice/manage/products/ProductSold.tsx
+++ src/components/pages/invoice/manage/products/ProductSold.tsx
@@ -58,10 +58,7 @@ export function ProductSoldBase({
 
   useEffect(() => {
     async function saveOutputsSolds() {
-      const isHumanChanges = Object.values(
-        someHumanChangesDetected.current
-      ).some((v) => v);
-
+      if (!invoiceDoc) return;
       let refresh_data;
       if (isPlainObject(invoiceDoc?.data()?.refresh_data))
         refresh_data = invoiceDoc?.data()?.refresh_data as Record<
@@ -70,47 +67,56 @@ export function ProductSoldBase({
         >;
       else refresh_data = {};
 
-      if (!invoiceDoc) return;
+      try {
+        const isHumanChanges = Object.values(
+          someHumanChangesDetected.current
+        ).some((v) => v);
 
-      if (refresh_data[product_doc.id])
-        if (!isHumanChanges) {
-          console.log("skip");
-          return;
+        if (refresh_data[product_doc.id])
+          if (!isHumanChanges) {
+            console.log("skip");
+            return;
+          }
+
+        console.log("save outputs solds");
+
+        const coll = collection(
+          invoiceDoc.ref,
+          "outputs_sold"
+        ) as CollectionReference<outputType>;
+
+        // disable the current outputs sold
+        const q = query(coll, where("disabled", "==", false));
+        const outputs_sold = await getDocs(q);
+
+        if (outputs_sold.size > 0) {
+          outputs_sold.forEach(async (doc) => {
+            await updateDoc(doc.ref, {
+              disabled: true,
+            });
+          });
         }
 
-      console.log("save outputs solds");
-
-      const coll = collection(
-        invoiceDoc.ref,
-        "outputs_sold"
-      ) as CollectionReference<outputType>;
-
-      // disable the current outputs sold
-      const q = query(coll, where("disabled", "==", false));
-      const outputs_sold = await getDocs(q);
-
-      if (outputs_sold.size > 0) {
-        outputs_sold.forEach(async (doc) => {
-          await updateDoc(doc.ref, {
-            disabled: true,
+        // outputs totalSold
+        await addOutputs(invoiceDoc, product_doc, remainStock, coll);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        if (
+          typeof refresh_data[product_doc.id] === "boolean" &&
+          refresh_data[product_doc.id] === false
+        )
+          // update the refresh data
+          await updateDoc(invoiceDoc.ref, {
+            [`refresh_data.${product_doc.id}`]: true,
           });
-        });
+
+        someHumanChangesDetected.current = {
+          addOutput: false,
+          devolution: false,
+          price: false,
+        };
       }
-
-      // outputs totalSold
-      await addOutputs(invoiceDoc, product_doc, remainStock, coll);
-
-      if (refresh_data[product_doc.id] === false)
-        // update the refresh data
-        await updateDoc(invoiceDoc.ref, {
-          [`refresh_data.${product_doc.id}`]: true,
-        });
-
-      someHumanChangesDetected.current = {
-        addOutput: false,
-        devolution: false,
-        price: false,
-      };
     }
 
     saveOutputsSolds();

e178dde9807f34a4049b1d58b3bf4042d7c60835
Carlos && Ponce <srtager555@gmail.com>
2025-05-13 17:45:10 -0600

    I starting to create the logic to show important data

--- src/pages/feed/index.tsx
+++ src/pages/feed/index.tsx
@@ -1,73 +1,123 @@
-import { Container } from "@/styles/index.styles";
+import { Container, FlexContainer } from "@/styles/index.styles";
 import type { NextPageWithLayout } from "../_app";
-import SalesComparisonChart from "@/components/chart";
+import SalesComparisonChart, { ChartData } from "@/components/chart";
+import { useEffect, useState } from "react";
+import { Firestore } from "@/tools/firestore";
+import {
+  collection,
+  CollectionReference,
+  getDocs,
+  query,
+  QueryDocumentSnapshot,
+  where,
+} from "firebase/firestore";
+import { getCurrentMonthRange } from "@/tools/time/current";
+import { InvoiceCollection } from "@/tools/firestore/CollectionTyping";
+import { invoiceType } from "@/tools/invoices/createInvoice";
+import styled from "styled-components";
+
+const WarnsContainer = styled(Container)`
+  width: 100%;
+`;
 
 const Feed: NextPageWithLayout = () => {
-  return (
-    <Container styles={{ width: "60vw" }}>
-      <h2>Venta de las ultimas 2 semanas</h2>
-      <Container
-        styles={{
-          width: "100%",
-          display: "grid",
-          // gridTemplateAreas: `"sells sells sells sells sells sells sells stats stats stats"`,
-          gridTemplateColumns: "repeat(10, 1fr)",
-          gridAutoRows: "50px",
-          gap: "20px",
-        }}
-      >
-        <Container
-          styles={{
-            // backgroundColor: "#ccc",
-            // gridArea: "sells",
-            gridColumn: "1 / -1",
-            gridRow: "1 / 8",
-            borderRadius: "20px",
-          }}
-        >
-          <SalesComparisonChart />
-        </Container>
-        {/* <Container
-          styles={{
-            backgroundColor: "#ccc",
-            // gridArea: "stats",
-            gridColumn: "9 / -1",
-            gridRow: "1 / 4",
-            borderRadius: "20px",
-          }}
-        /> */}
-        <Container
-          styles={{
-            backgroundColor: "#ccc",
-            gridArea: "stats",
-            gridColumn: "1 / -1",
-            gridRow: "8 / 10",
-            borderRadius: "20px",
-          }}
-        />
-        <Container
-          styles={{
-            backgroundColor: "#ccc",
-            gridArea: "stats",
-            gridColumn: "1 / -1",
-            gridRow: "11 / 15",
-            borderRadius: "20px",
-          }}
-        />
+  const [chartData, setChartData] = useState<ChartData>([]);
+  const [overdueInvoices, setOverdueInvoices] = useState<
+    QueryDocumentSnapshot<invoiceType>[]
+  >([]);
 
-        {/* <Container
-        styles={{
-          display: "grid",
-          gridTemplateColumns: "repeat(6, 1fr)",
-          gridAutoRows: "100px",
-          backgroundColor: "#c1c",
-        }}
-      >
-      </Container> */}
+  // effect to get the sales
+  useEffect(() => {
+    async function getSales() {
+      const db = Firestore();
+      const coll = collection(
+        db,
+        InvoiceCollection.root
+      ) as CollectionReference<invoiceType>;
+      const range = getCurrentMonthRange(new Date(), 2);
+      console.log(range);
+
+      const q = query(
+        coll,
+        where("disabled", "==", false),
+        where("created_at", "<=", range.end),
+        where("created_at", ">=", range.start)
+      );
+
+      const snap = await getDocs(q);
+
+      console.log("invoices", snap.docs);
+
+      const data = snap.docs.map((doc) => {
+        return {
+          createdAt: doc.data().created_at?.toDate() as Date,
+          amount: Number(doc.data().total_sold.toFixed(2)),
+        };
+      });
+      console.log(data);
+
+      setChartData(data);
+    }
+
+    getSales();
+  }, []);
+
+  // Effect to get the overdue invoices
+  useEffect(() => {
+    async function getOverdueInvoices() {
+      const db = Firestore();
+      const coll = collection(
+        db,
+        InvoiceCollection.root
+      ) as CollectionReference<invoiceType> as CollectionReference<invoiceType>;
+
+      const q = query(coll, where("credit.paid", "==", false));
+      const docs = getDocs(q);
+
+      setOverdueInvoices(docs);
+    }
+
+    getOverdueInvoices();
+  }, []);
+
+  return (
+    <Container styles={{ maxWidth: "1500px", marginTop: "30px" }}>
+      <h1 style={{ textAlign: "center" }}>Ventas del mes</h1>
+
+      <Container styles={{ marginBottom: "30px" }}>
+        <SalesComparisonChart
+          invoiceDataToChart={chartData}
+          numberOfDaysToShow={30}
+        />
       </Container>
+
+      <FlexContainer styles={{ justifyContent: "space-between", gap: "10px" }}>
+        <WarnsContainer>
+          <h2>Facturas vencidas</h2>
+          <Container>
+            {overdueInvoices.length > 0 ? (
+              overdueInvoices.map((doc, i) => {
+                const data = doc.data();
+                return <Container key={i}></Container>;
+              })
+            ) : (
+              <p>Todo en orden</p>
+            )}
+          </Container>
+        </WarnsContainer>
+        <WarnsContainer>
+          <h2>Facturas que necesitan revisión</h2>
+          <Container>
+            <p>Todo en orden</p>
+          </Container>
+        </WarnsContainer>
+      </FlexContainer>
+
       <p>There is the Feed</p>
     </Container>
   );
 };
 
 export default Feed;
+
+export function OverdueInvoicePreview() {}

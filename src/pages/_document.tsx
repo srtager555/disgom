ac7d78db8c898d00252651d4c3e5abf1d604c79b
Carlos && Ponce <srtager555@gmail.com>
2025-05-25 11:09:45 -0600

    Manifest added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icons/icon-192x192.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />

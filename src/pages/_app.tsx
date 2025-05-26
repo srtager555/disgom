7c646b8e487d1960893b05c798ce5bc3e162529c
Carlos && Ponce <srtager555@gmail.com>
2025-05-26 16:54:20 -0600

    service worker commented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { Layout } from "@/components/layouts/index.layout";
 import "@/styles/globals.css";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
-import { ReactElement, ReactNode, useEffect } from "react";
+import { ReactElement, ReactNode } from "react";
 
 export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -16,18 +16,18 @@ export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  useEffect(() => {
-    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js")
-        .then((registration) => {
-          console.log("Service Worker registrado con éxito:", registration);
-        })
-        .catch((error) => {
-          console.error("Error al registrar el Service Worker:", error);
-        });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+  //     navigator.serviceWorker
+  //       .register("/service-worker.js")
+  //       .then((registration) => {
+  //         console.log("Service Worker registrado con éxito:", registration);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error al registrar el Service Worker:", error);
+  //       });
+  //   }
+  // }, []);
 
   return <Layout>{getLayout(<Component {...pageProps} />)}</Layout>;
 }

4a5352f245a771471ffbaa171adafed5f31c83e8
Carlos && Ponce <srtager555@gmail.com>
2025-05-29 17:34:32 -0600

    issue about infinite loop fixed

--- src/hooks/invoice/useHasNextInvoice.ts
+++ src/hooks/invoice/useHasNextInvoice.ts
@@ -3,7 +3,7 @@ import { useInvoice } from "@/contexts/InvoiceContext";
 import { HasInvoice } from "@/pages/invoices/manage";
 import { invoiceType } from "@/tools/invoices/createInvoice";
 import { PartialWithFieldValue, updateDoc } from "firebase/firestore";
-import { useContext, useState } from "react";
+import { useCallback, useContext, useState } from "react";
 
 export function useHasNextInvoice() {
   // Mantén esta línea como está, el cambio es en la función interna.
@@ -11,25 +11,24 @@ export function useHasNextInvoice() {
   const { invoice } = useInvoice();
   const [map, setMap] = useState<Map<string, boolean>>(new Map());
 
-  async function checkHasNextInvoice(
-    fn: any,
-    humanChange: boolean,
-    product_id: string
-  ) {
-    const data = invoice?.data();
-    if (data?.next_invoice_ref && humanChange) {
-      setHasInvoice(true);
-      const newMap = new Map(map);
-      newMap.set(product_id, true);
-      setMap(newMap);
+  const checkHasNextInvoice = useCallback(
+    async (fn: any, humanChange: boolean, product_id: string) => {
+      const data = invoice?.data();
+      if (data?.next_invoice_ref && humanChange) {
+        setHasInvoice(true);
+        const newMap = new Map(map);
+        newMap.set(product_id, true);
+        setMap(newMap);
 
-      await updateDoc(data.next_invoice_ref, {
-        refresh_data: Object.fromEntries(map),
-      } as PartialWithFieldValue<invoiceType>);
-    }
+        await updateDoc(data.next_invoice_ref, {
+          refresh_data: Object.fromEntries(map),
+        } as PartialWithFieldValue<invoiceType>);
+      }
 
-    return fn();
-  }
+      return fn();
+    },
+    [invoice, map, setHasInvoice]
+  );
 
   return { checkHasNextInvoice };
 }

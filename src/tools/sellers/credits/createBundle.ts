0f0e9a3b76ef6a20da2dff665502215f9647032c
Carlos && Ponce <srtager555@gmail.com>
2025-06-01 02:50:17 -0600

    issue about get the previus bundle fixed

--- src/tools/sellers/credits/createBundle.ts
+++ src/tools/sellers/credits/createBundle.ts
@@ -4,10 +4,6 @@ import {
   collection,
   CollectionReference,
   DocumentReference,
-  getDocs,
-  limit,
-  orderBy,
-  query,
   Timestamp,
   updateDoc,
 } from "firebase/firestore";
@@ -26,32 +22,24 @@ export type creditBundle = {
 interface props {
   seller_ref: DocumentReference<SellersDoc>;
   invoice_ref: DocumentReference<invoiceType>;
+  previus_bundle_ref: DocumentReference<creditBundle> | null;
 }
 
 export async function createCreditBundle({
   seller_ref,
   invoice_ref,
+  previus_bundle_ref,
 }: props): Promise<DocumentReference<creditBundle>> {
   const coll = collection(
     seller_ref,
     SellersCollection.creditBundles
   ) as CollectionReference<creditBundle>;
 
-  let last_bundle_ref: DocumentReference<creditBundle> | null = null;
-
-  const q = query(coll, orderBy("created_at", "desc"), limit(1));
-  const querySnapshot = await getDocs(q);
-
-  if (!querySnapshot.empty) {
-    last_bundle_ref = querySnapshot.docs[0]
-      .ref as DocumentReference<creditBundle>;
-  }
-
   const newBundleData: creditBundle = {
     created_at: Timestamp.fromDate(new Date()),
     seller_ref,
     invoice_ref,
-    last_bundle: last_bundle_ref,
+    last_bundle: previus_bundle_ref,
     next_bundle: null,
     disabled: false,
   };
@@ -64,8 +52,8 @@ export async function createCreditBundle({
   });
 
   // If there was a previous bundle, update its next_bundle field
-  if (last_bundle_ref) {
-    await updateDoc(last_bundle_ref, {
+  if (previus_bundle_ref) {
+    await updateDoc(previus_bundle_ref, {
       next_bundle: newBundleRef,
     });
   }

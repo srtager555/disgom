8f3881920e153c5278413905f309f8e511c40e19
Carlos && Ponce <srtager555@gmail.com>
2025-06-03 23:31:59 -0600

    credit bundle container logic added

--- src/tools/sellers/credits/createBundle.ts
+++ src/tools/sellers/credits/createBundle.ts
@@ -16,29 +16,55 @@ export type creditBundle = {
   invoice_ref: DocumentReference<invoiceType>;
   last_bundle: DocumentReference<creditBundle> | null;
   next_bundle: DocumentReference<creditBundle> | null;
+  bundle_container_ref: DocumentReference<creditBundleContainerDoc>;
   disabled: boolean; // Campo para marcar el bundle como deshabilitado
 };
 
-interface props {
+interface creditBundleprops {
   seller_ref: DocumentReference<SellersDoc>;
   invoice_ref: DocumentReference<invoiceType>;
   previus_bundle_ref: DocumentReference<creditBundle> | null;
+  bundle_container_ref: DocumentReference<creditBundleContainerDoc>;
+}
+
+export interface creditBundleContainerDoc {
+  disabled: boolean;
+  created_at: Timestamp;
+  seller_ref: DocumentReference<SellersDoc>;
+}
+
+export async function createCreditBundleContainer(
+  seller_ref: DocumentReference<SellersDoc>
+): Promise<DocumentReference<creditBundleContainerDoc>> {
+  return await addDoc(
+    collection(
+      seller_ref,
+      SellersCollection.creditBundles.root
+    ) as CollectionReference<creditBundleContainerDoc>,
+    {
+      disabled: false,
+      created_at: Timestamp.fromDate(new Date()),
+      seller_ref,
+    }
+  );
 }
 
 export async function createCreditBundle({
   seller_ref,
   invoice_ref,
   previus_bundle_ref,
-}: props): Promise<DocumentReference<creditBundle>> {
+  bundle_container_ref,
+}: creditBundleprops): Promise<DocumentReference<creditBundle>> {
   const coll = collection(
-    seller_ref,
-    SellersCollection.creditBundles
+    bundle_container_ref,
+    SellersCollection.creditBundles.bundles.root
   ) as CollectionReference<creditBundle>;
 
   const newBundleData: creditBundle = {
     created_at: Timestamp.fromDate(new Date()),
     seller_ref,
     invoice_ref,
+    bundle_container_ref,
     last_bundle: previus_bundle_ref,
     next_bundle: null,
     disabled: false,

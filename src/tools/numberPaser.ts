677b1e362d2bfa4f99e53939125846b766a5610b
Carlos && Ponce <srtager555@gmail.com>
2025-06-07 15:21:04 -0600

    now the number parse show a "-" insteace of a "0"

--- src/tools/numberPaser.ts
+++ src/tools/numberPaser.ts
@@ -1,6 +1,12 @@
-export function numberParser(n: number) {
-  return n.toLocaleString(undefined, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  });
+export function numberParser(n: number, addMoneySign = false) {
+  if (n === 0) {
+    return "-";
+  } else {
+    const result = n.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+
+    return addMoneySign ? `L ${result}` : result;
+  }
 }

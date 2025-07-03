9d23ac7985387e95d0169a2786680d123a255df5
Carlos && Ponce <srtager555@gmail.com>
2025-07-03 15:55:56 -0600

    new conditional to manage the persistent prices added

--- src/tools/products/addEntry.ts
+++ src/tools/products/addEntry.ts
@@ -3,7 +3,9 @@ import {
   collection,
   doc,
   DocumentReference,
+  DocumentSnapshot,
   Timestamp,
+  updateDoc,
 } from "firebase/firestore";
 import { productDoc } from "./create";
 import { ProductsCollection } from "../firestore/CollectionTyping";
@@ -25,10 +27,10 @@ export interface entryDoc extends entryData {
 }
 
 export async function addEntry(
-  productRef: DocumentReference<productDoc>,
+  product: DocumentSnapshot<productDoc>,
   data: entryData
 ) {
-  const coll = collection(productRef, ProductsCollection.entry);
+  const coll = collection(product.ref, ProductsCollection.entry);
 
   await addDoc(coll, {
     created_at: new Date(),
@@ -37,10 +39,26 @@ export async function addEntry(
   }).then(async (_) => {
     const entry_ref = doc(coll, _.id) as DocumentReference<entryDoc>;
 
-    await addToStock(productRef, {
+    const last_sales_amounts = product.data()?.last_sales_amounts;
+    const purchaseIsEqual =
+      last_sales_amounts?.purchase_price === data.purchase_price;
+    const saleIsEqual = last_sales_amounts?.sale_price === data.sale_price;
+
+    // update the persistent prices
+    if (!purchaseIsEqual || !saleIsEqual) {
+      await updateDoc(product.ref, {
+        last_sales_amounts: {
+          purchase_price: data.purchase_price,
+          sale_price: data.sale_price,
+          seller_commission: data.seller_commission,
+        },
+      });
+    }
+
+    await addToStock(product.ref, {
       entry_ref,
       ...data,
-      product_ref: productRef,
+      product_ref: product.ref,
     });
   });
 }

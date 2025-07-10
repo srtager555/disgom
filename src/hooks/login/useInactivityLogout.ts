ae59d4f362969b69bae9b7924de3788c3b72a78a
Carlos && Ponce <srtager555@gmail.com>
2025-07-10 16:56:31 -0600

    issue about run the inactivity callback fixed

--- src/hooks/login/useInactivityLogout.ts
+++ src/hooks/login/useInactivityLogout.ts
@@ -1,35 +1,23 @@
-import { useCallback, useEffect, useRef } from "react";
 import { getAuth, signOut } from "firebase/auth";
 import { useRouter } from "next/router";
+import { useIdleTimer } from "react-idle-timer";
 
 const INACTIVITY_LIMIT = 5 * 60 * 1000; // 5 minutos
 
 export function useInactivityLogout() {
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
   const router = useRouter();
 
-  const resetTimer = useCallback(() => {
-    const auth = getAuth();
+  useIdleTimer({
+    timeout: INACTIVITY_LIMIT,
+    onIdle: () => {
+      const auth = getAuth();
 
-    if (timeoutRef.current) clearTimeout(timeoutRef.current);
-    timeoutRef.current = setTimeout(() => {
       if (auth.currentUser) {
         console.log("Usuario inactivo, cerrando sesión.");
         signOut(auth);
 
         router.push("/");
       }
-    }, INACTIVITY_LIMIT);
-  }, []);
-
-  useEffect(() => {
-    const events = ["mousemove", "keydown", "scroll", "touchstart"];
-    events.forEach((event) => window.addEventListener(event, resetTimer));
-    resetTimer();
-
-    return () => {
-      if (timeoutRef.current) clearTimeout(timeoutRef.current);
-      events.forEach((event) => window.removeEventListener(event, resetTimer));
-    };
-  }, [resetTimer]);
+    },
+  });
 }

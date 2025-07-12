2e1093b5b28247ab55b2ae14189bd83ea7a32d7a
Carlos && Ponce <srtager555@gmail.com>
2025-07-11 22:14:51 -0600

    one time get added

--- src/hooks/products/getEntries.ts
+++ src/hooks/products/getEntries.ts
@@ -1,12 +1,11 @@
 import { ProductsCollection } from "@/tools/firestore/CollectionTyping";
 import { entryDoc } from "@/tools/products/addEntry";
 import { productDoc } from "@/tools/products/create";
-import { getCurrentTwoWeekRange } from "@/tools/time/current";
 import {
   collection,
   CollectionReference,
   DocumentReference,
-  onSnapshot,
+  getDocs,
   query,
   QueryDocumentSnapshot,
   where,
@@ -18,29 +17,32 @@ export function useGetEntries(ref: DocumentReference<productDoc> | undefined) {
   const last_ref_id = useRef("");
 
   useEffect(() => {
-    if (!ref) return;
-    if (last_ref_id.current === ref.id) return;
+    async function getEntries() {
+      if (!ref) return;
+      if (last_ref_id.current === ref.id) return;
 
-    last_ref_id.current = ref.id;
+      last_ref_id.current = ref.id;
 
-    const coll = collection(
-      ref,
-      ProductsCollection.entry
-    ) as CollectionReference<entryDoc>;
+      const coll = collection(
+        ref,
+        ProductsCollection.entry
+      ) as CollectionReference<entryDoc>;
 
-    const range = getCurrentTwoWeekRange(new Date());
-    const q = query(
-      coll,
-      where("disabled", "==", false),
-      where("created_at", ">=", range.start),
-      where("created_at", "<=", range.end)
-    );
+      const now = new Date();
+      const _14DaysAgo = new Date(now.getTime() - 14 * 24 * 60 * 60 * 1000);
 
-    const unsubcribe = onSnapshot(q, (entries) => {
-      setEntries(entries.docs);
-    });
+      const q = query(
+        coll,
+        where("disabled", "==", false),
+        where("created_at", ">=", _14DaysAgo),
+        where("created_at", "<=", now)
+      );
 
-    return () => unsubcribe();
+      const entriesQuery = await getDocs(q);
+      setEntries(entriesQuery.docs);
+    }
+
+    getEntries();
   }, [ref]);
 
   return entries;

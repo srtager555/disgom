b14a463c7d583a2bcf92d3099e3d4c7e74542b60
Carlos && Ponce <srtager555@gmail.com>
2025-07-12 14:25:57 -0600

    Added missing attribute that throw an error atomic operation

--- src/hooks/invoice/useManageOutputs.ts
+++ src/hooks/invoice/useManageOutputs.ts
@@ -157,6 +157,7 @@ export function useManageOutputs({
           currentOutputsServerAmount,
           productParentData,
           setRawOutputs, // Pass setRawOutputs to update UI immediately
+          undefined,
           priceToSave
         ); // Increase amount
       } else if (amountToSave > currentOutputsServerAmount) {

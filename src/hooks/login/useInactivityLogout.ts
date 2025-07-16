76a3226a4a071299df0775ff40eefb913a539e31
Carlos && Ponce <srtager555@gmail.com>
2025-07-15 23:34:01 -0600

    hook commented

--- src/hooks/login/useInactivityLogout.ts
+++ src/hooks/login/useInactivityLogout.ts
@@ -1,23 +1,20 @@
-import { getAuth, signOut } from "firebase/auth";
-import { useRouter } from "next/router";
-import { useIdleTimer } from "react-idle-timer";
+// import { getAuth, signOut } from "firebase/auth";
+// import { useRouter } from "next/router";
+// import { useIdleTimer } from "react-idle-timer";
 
-const INACTIVITY_LIMIT = 5 * 60 * 1000; // 5 minutos
+// const INACTIVITY_LIMIT = 5 * 60 * 1000; // 5 minutos
 
 export function useInactivityLogout() {
-  const router = useRouter();
-
-  useIdleTimer({
-    timeout: INACTIVITY_LIMIT,
-    onIdle: () => {
-      const auth = getAuth();
-
-      if (auth.currentUser) {
-        console.log("Usuario inactivo, cerrando sesión.");
-        signOut(auth);
-
-        router.push("/");
-      }
-    },
-  });
+  // const router = useRouter();
+  // useIdleTimer({
+  //   timeout: INACTIVITY_LIMIT,
+  //   onIdle: () => {
+  //     const auth = getAuth();
+  //     if (auth.currentUser) {
+  //       console.log("Usuario inactivo, cerrando sesión.");
+  //       signOut(auth);
+  //       router.push("/");
+  //     }
+  //   },
+  // });
 }

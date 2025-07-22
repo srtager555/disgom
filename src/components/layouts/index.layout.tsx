3a4012bf4e0f8e5aa979c06195c4efd41ddbcca1
Carlos && Ponce <srtager555@gmail.com>
2025-07-21 23:23:08 -0600

    new layout added to manage the invoices when the PWA is offline

--- src/components/layouts/index.layout.tsx
+++ src/components/layouts/index.layout.tsx
@@ -2,16 +2,19 @@ import { InitApp } from "@/firebase/InitApp";
 import { NavLayout } from "./nav.layout";
 import { Container } from "@/styles/index.styles";
 import { LoginLayout } from "./login.layout";
+import { OfflineInvoiceIDManager } from "./OfflineInvoiceIDManager.layout";
 
 export function Layout({ children }: { children: children }) {
   return (
     <InitApp>
       <LoginLayout>
-        <NavLayout>
-          <Container styles={{ padding: "0 2.5%", margin: "0 auto" }}>
-            {children}
-          </Container>
-        </NavLayout>
+        <OfflineInvoiceIDManager>
+          <NavLayout>
+            <Container styles={{ padding: "0 2.5%", margin: "0 auto" }}>
+              {children}
+            </Container>
+          </NavLayout>
+        </OfflineInvoiceIDManager>
       </LoginLayout>
     </InitApp>
   );

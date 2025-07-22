2c30debd375f6e5ef3ed70e094cf4466ec20bb5e
Carlos && Ponce <srtager555@gmail.com>
2025-07-21 23:25:31 -0600

    Small changes to improve the function

--- src/tools/invoices/createInvoice.ts
+++ src/tools/invoices/createInvoice.ts
@@ -11,10 +11,7 @@ import {
   where,
 } from "firebase/firestore";
 import { Firestore } from "../firestore";
-import {
-  InvoiceCollection,
-  SellersCollection,
-} from "../firestore/CollectionTyping";
+import { InvoiceCollection } from "../firestore/CollectionTyping";
 import { SellersDoc } from "../sellers/create";
 import { client } from "../sellers/createClient";
 import { outputType } from "../products/addOutputs";
@@ -22,7 +19,6 @@ import { inventory } from "../sellers/invetory/create";
 import { rawMoneyType } from "@/components/pages/invoice/manage/Closing/Money";
 import { createAnormalInvoice } from "./createAnormalInvoice";
 import { creditBundle } from "../sellers/credits/createBundle";
-import { getDocFromCacheOnce } from "../firestore/fetch/getDocFromCacheOnce";
 import { getQueryFromCacheOnce } from "../firestore/fetch/getQueryFromCacheOnce";
 
 export type invoiceType = {
@@ -33,7 +29,7 @@ export type invoiceType = {
   products_outputs: Record<string, Array<DocumentReference<outputType>>>;
   outputs_sold: Record<string, Array<DocumentReference<outputType>>>;
   devolution: DocumentReference<inventory> | null;
-  last_inventory_ref: DocumentReference<inventory> | null;
+  // last_inventory_ref: DocumentReference<inventory> | null;
   next_invoice_ref: DocumentReference<invoiceType> | null;
   prev_invoice_ref: DocumentReference<invoiceType> | null;
   total_sold: number;
@@ -64,80 +60,100 @@ export async function createInvoice(
   >,
   fromAnormalInvoiceProcess?: boolean
 ): Promise<DocumentReference<invoiceType> | undefined> {
-  const { seller_ref, invoice_type = "normal" } = data;
-  const db = Firestore();
-  const coll = collection(
-    db,
-    InvoiceCollection.root
-  ) as CollectionReference<invoiceType>;
-  let last_inventory = null;
+  try {
+    console.log("creating invoice...");
+    const { seller_ref, invoice_type = "normal" } = data;
+    const db = Firestore();
+    const coll = collection(
+      db,
+      InvoiceCollection.root
+    ) as CollectionReference<invoiceType>;
+    // let last_inventory = null;
 
-  if (!fromAnormalInvoiceProcess) {
-    if (invoice_type !== "normal" && invoice_type !== undefined) {
-      return createAnormalInvoice(invoice_type);
-    } else if (seller_ref) {
-      const sellerDoc = await getDocFromCacheOnce(seller_ref);
-
-      if (sellerDoc.data()?.hasInventory) {
-        const coll = collection(
-          sellerDoc.ref,
-          SellersCollection.inventories.root
-        ) as CollectionReference<inventory>;
-        const q = query(coll, orderBy("created_at", "desc"), limit(1));
-        const inventories = await getQueryFromCacheOnce(q);
-
-        last_inventory = inventories.docs[0];
+    if (!fromAnormalInvoiceProcess) {
+      console.log("Is from anormal invoice process?");
+      if (invoice_type !== "normal" && invoice_type !== undefined) {
+        console.log("Creating anormal invoice...");
+        return await createAnormalInvoice(invoice_type);
       }
+      // else if (seller_ref) {
+      //   console.log("Is a normal invoice")
+      //   const sellerDoc = await getDocFromCacheOnce(seller_ref);
+
+      //   if (sellerDoc.data()?.hasInventory) {
+      //     console.log("Getting last inventory...")
+      //     const coll = collection(
+      //       sellerDoc.ref,
+      //       SellersCollection.inventories.root
+      //     ) as CollectionReference<inventory>;
+      //     const q = query(coll, orderBy("created_at", "desc"), limit(1));
+      //     const inventories = await getQueryFromCacheOnce(q);
+
+      //     last_inventory = inventories.docs[0];
+      //   }
+      // }
     }
-  }
 
-  if (!seller_ref) return;
-  let lastInvoice = null;
+    if (!seller_ref) {
+      console.log("No seller ref defined");
+      return;
+    }
+    let lastInvoice = null;
 
-  if (invoice_type === "normal" && !data.client_ref) {
-    const queryLastInvoice = query(
-      coll,
-      orderBy("created_at", "desc"),
-      where("disabled", "==", false),
-      where("seller_ref", "==", seller_ref),
-      limit(1)
-    );
-    lastInvoice = await getQueryFromCacheOnce(queryLastInvoice);
-  }
+    if (invoice_type === "normal" && !data.client_ref) {
+      console.log("Getting last invoice...");
+      const queryLastInvoice = query(
+        coll,
+        orderBy("created_at", "desc"),
+        where("disabled", "==", false),
+        where("seller_ref", "==", seller_ref),
+        limit(1)
+      );
+      lastInvoice = await getQueryFromCacheOnce(queryLastInvoice);
+    }
 
-  const newInvoice = await addDoc(coll, {
-    created_at: Timestamp.fromDate(new Date()),
-    seller_ref,
-    client_ref: data.client_ref ?? null,
-    products_outputs: {},
-    devolution: null,
-    outputs_sold: {},
-    total_sold: 0,
-    total_cost: 0,
-    total_proft: 0,
-    route: null,
-    bills: 0,
-    money: {},
-    diff: {
-      amount: 0,
-      paid: false,
-      paid_at: null,
-    },
-    credit: null,
-    credit_bundle_ref: null,
-    disabled: false,
-    delete_at: null,
-    last_inventory_ref: last_inventory?.ref || null,
-    next_invoice_ref: null,
-    prev_invoice_ref: lastInvoice?.docs[0]?.ref || null,
-    refresh_data: null,
-    invoice_type,
-  });
-
-  if (lastInvoice?.docs[0])
-    await updateDoc(lastInvoice.docs[0].ref, {
-      next_invoice_ref: newInvoice,
+    console.log("Creating invoice...");
+    const newInvoice = await addDoc(coll, {
+      created_at: Timestamp.fromDate(new Date()),
+      seller_ref,
+      client_ref: data.client_ref ?? null,
+      products_outputs: {},
+      devolution: null,
+      outputs_sold: {},
+      total_sold: 0,
+      total_cost: 0,
+      total_proft: 0,
+      route: null,
+      bills: 0,
+      money: {},
+      diff: {
+        amount: 0,
+        paid: false,
+        paid_at: null,
+      },
+      credit: null,
+      credit_bundle_ref: null,
+      disabled: false,
+      delete_at: null,
+      // last_inventory_ref: last_inventory?.ref || null,
+      next_invoice_ref: null,
+      prev_invoice_ref: lastInvoice?.docs[0]?.ref || null,
+      refresh_data: null,
+      invoice_type,
     });
 
-  return newInvoice;
+    if (lastInvoice?.docs[0]) {
+      console.log("updating the last invoice with the new invoice");
+      await updateDoc(lastInvoice.docs[0].ref, {
+        next_invoice_ref: newInvoice,
+      });
+    }
+
+    // returning the new invoice
+    return newInvoice;
+  } catch (error) {
+    console.log("error creating invoice");
+    console.log(error);
+    return;
+  }
 }

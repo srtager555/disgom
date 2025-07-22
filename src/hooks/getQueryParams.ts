4725b09a56f4b8716f446b70867777feb6e7bd42
Carlos && Ponce <srtager555@gmail.com>
2025-07-21 23:24:31 -0600

    some changes to work with the PWA

--- src/hooks/getQueryParams.ts
+++ src/hooks/getQueryParams.ts
@@ -1,22 +1,32 @@
 import { useEffect, useState } from "react";
+import { useInvoiceIDManager } from "./offline/InvoiceIDManager";
 
 function useQueryParams() {
   const [queryParams, setQueryParams] = useState<Record<string, string>>({});
+  const { invoiceID } = useInvoiceIDManager();
 
   useEffect(() => {
+    const result: Record<string, string> = {};
     if (global?.window && window.location.search) {
       const params = new URLSearchParams(window.location.search);
-      const result: Record<string, string> = {};
 
       params.forEach((value, key) => {
         result[key] = value;
       });
 
       setQueryParams(result);
+    } else {
+      if (invoiceID) {
+        result["id"] = invoiceID;
+      }
+      setQueryParams(result);
     }
 
+    console.log("invoice id manager", invoiceID);
+    console.log("query results", result);
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [global?.window, global?.window?.location.search]);
+  }, [global?.window, global?.window?.location.search, invoiceID]);
 
   return queryParams;
 }
